f4ebfe5f9543747ffac4e4f96c601c1e5406e145
Alexis Guzman <codingcodax@gmail.com>
2024-08-01 12:34:00 -0600

    feat(components): add `<Tab />` and `<Tabs />`

--- apps/www/src/mdx-components.tsx
+++ apps/www/src/mdx-components.tsx
@@ -1,9 +1,12 @@
 import type { MDXComponents } from 'mdx/types';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import defaultComponents from 'fumadocs-ui/mdx';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    Tab,
+    Tabs,
   };
 }
