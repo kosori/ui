d6ce4f8cbe0a8321c1780dd4db46efb282898287
Alexis Guzman <codingcodax@gmail.com>
2024-08-01 10:55:55 -0600

    feat(layout): create base options

---
+++ apps/www/src/app/layout.config.ts
@@ -0,0 +1,18 @@
+import { type HomeLayoutProps } from 'fumadocs-ui/home-layout';
+import { type DocsLayoutProps } from 'fumadocs-ui/layout';
+
+import { pageTree } from '~/app/source';
+
+// shared configuration
+export const baseOptions: HomeLayoutProps = {
+  nav: {
+    title: 'kosori/ui',
+  },
+  githubUrl: 'https://github.com/kosori/ui',
+};
+
+// docs layout configuration
+export const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: pageTree,
+};
