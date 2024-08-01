b8197e620c66b9e5087eb28703f2e626e42dd2fc
Alexis Guzman <codingcodax@gmail.com>
2024-08-01 10:57:47 -0600

    feat(layout): create with docs layout

---
+++ apps/www/src/app/docs/layout.tsx
@@ -0,0 +1,8 @@
+import type { ReactNode } from 'react';
+import { DocsLayout } from 'fumadocs-ui/layout';
+
+import { docsOptions } from '../layout.config';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
+}
