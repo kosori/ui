586f426a7ba27def303c03bd8588e90083e3f7c1
Alexis Guzman <codingcodax@gmail.com>
2024-08-01 10:54:14 -0600

    feat(tailwind): add fumadocs ui styles

--- apps/www/tailwind.config.ts
+++ apps/www/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 
 import baseConfig from '@kosori/tailwind-config/web';
@@ -6,8 +7,12 @@ import baseConfig from '@kosori/tailwind-config/web';
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
-  content: [...baseConfig.content, '../../packages/ui/**/*.{ts,tsx}'],
-  presets: [baseConfig],
+  content: [
+    ...baseConfig.content,
+    '../../packages/ui/**/*.{ts,tsx}',
+    '../../node_modules/fumadocs-ui/dist/**/*.js',
+  ],
+  presets: [baseConfig, createPreset()],
   theme: {
     extend: {
       fontFamily: {
