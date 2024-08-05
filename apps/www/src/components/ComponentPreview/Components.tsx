8bf612a92476b62aea7c12d85c4faad52d958ded
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 13:52:21 -0600

    feat(components): add accordion and alert demos

---
+++ apps/www/src/components/ComponentPreview/Components.tsx
@@ -0,0 +1,64 @@
+import { lazy } from 'react';
+
+type Component = {
+  name: string;
+  type: 'component:example';
+  component: React.LazyExoticComponent<() => JSX.Element>;
+};
+
+export const Components: Record<string, Component> = {
+  accordion: {
+    name: 'accordion',
+    type: 'component:example',
+    component: lazy(() =>
+      import('../demos/Accordion').then((module) => ({
+        default: module.AccordionDemo,
+      })),
+    ),
+  },
+  alert: {
+    name: 'alert',
+    type: 'component:example',
+    component: lazy(() =>
+      import('../demos/Alert').then((module) => ({
+        default: module.AlertDemo,
+      })),
+    ),
+  },
+  'alert-error': {
+    name: 'alert-error',
+    type: 'component:example',
+    component: lazy(() =>
+      import('../demos/Alert').then((module) => ({
+        default: module.AlertErrorDemo,
+      })),
+    ),
+  },
+  'alert-info': {
+    name: 'alert-info',
+    type: 'component:example',
+    component: lazy(() =>
+      import('../demos/Alert').then((module) => ({
+        default: module.AlertInfoDemo,
+      })),
+    ),
+  },
+  'alert-success': {
+    name: 'alert-success',
+    type: 'component:example',
+    component: lazy(() =>
+      import('../demos/Alert').then((module) => ({
+        default: module.AlertSuccessDemo,
+      })),
+    ),
+  },
+  'alert-warning': {
+    name: 'alert-warning',
+    type: 'component:example',
+    component: lazy(() =>
+      import('../demos/Alert').then((module) => ({
+        default: module.AlertWarningDemo,
+      })),
+    ),
+  },
+};
