4700b7eb186e2bc5e51534a72c7654fc3e6ac448
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 17:48:09 -0600

    feat(components): create button variants demo

---
+++ apps/www/src/components/demos/Button/ButtonVariants.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@kosori/ui/Button';
+
+export const ButtonVariantsDemo = () => {
+  return (
+    <div className='space-y-4'>
+      <div className='flex gap-2'>
+        <Button>Solid</Button>
+        <Button variant='soft'>Soft</Button>
+        <Button variant='outline'>Outline</Button>
+        <Button variant='ghost'>Ghost</Button>
+      </div>
+
+      <div className='flex gap-2'>
+        <Button intent='danger'>Solid</Button>
+        <Button intent='danger' variant='soft'>
+          Soft
+        </Button>
+        <Button intent='danger' variant='outline'>
+          Outline
+        </Button>
+        <Button intent='danger' variant='ghost'>
+          Ghost
+        </Button>
+      </div>
+    </div>
+  );
+};
