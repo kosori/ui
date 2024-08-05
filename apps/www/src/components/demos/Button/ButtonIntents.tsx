d0000e545f312557430ccfa2c36b6c9ce9405f4d
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 17:47:59 -0600

    feat(components): create button intents demo

---
+++ apps/www/src/components/demos/Button/ButtonIntents.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@kosori/ui/Button';
+
+export const ButtonIntentsDemo = () => {
+  return (
+    <div className='flex gap-2'>
+      <Button>Button</Button>
+      <Button intent='danger'>Button</Button>
+    </div>
+  );
+};
