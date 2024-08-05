41475e44f29d2f76b0945940dfe150c00eee0cf6
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 14:14:06 -0600

    feat(components): add component preview

--- apps/www/src/mdx-components.tsx
+++ apps/www/src/mdx-components.tsx
@@ -2,11 +2,14 @@ import type { MDXComponents } from 'mdx/types';
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import defaultComponents from 'fumadocs-ui/mdx';
 
+import { ComponentPreview } from '~/components/ComponentPreview';
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
     Tab,
     Tabs,
+    ComponentPreview,
   };
 }
