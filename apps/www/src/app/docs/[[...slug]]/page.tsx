23cdd077235b292fe1e28d648c61a9baa84e4d42
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 13:12:27 -0600

    feat(pages): add `docs` and `api` badge links

--- apps/www/src/app/docs/[[...slug]]/page.tsx
+++ apps/www/src/app/docs/[[...slug]]/page.tsx
@@ -1,15 +1,35 @@
-import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import { ExternalLinkIcon } from '@radix-ui/react-icons';
 import { DocsBody, DocsPage } from 'fumadocs-ui/page';
 
-import { getPage, getPages } from '~/app/source';
+import { Badge } from '@kosori/ui/badge';
 
-export default async function Page({
+import { docs } from '~/app/source';
+
+export const generateStaticParams = () => {
+  return docs.getPages().map((page) => ({
+    slug: page.slugs,
+  }));
+};
+
+export const generateMetadata = ({
   params,
 }: {
   params: { slug?: string[] };
-}) {
-  const page = getPage(params.slug);
+}) => {
+  const page = docs.getPage(params.slug);
+
+  if (page == null) notFound();
+
+  return {
+    title: page.data.title,
+    description: page.data.description,
+    links: page.data.links,
+  };
+};
+
+const Page = ({ params }: { params: { slug?: string[] } }) => {
+  const page = docs.getPage(params.slug);
 
   if (page == null) {
     notFound();
@@ -18,29 +38,39 @@ export default async function Page({
   const MDX = page.data.exports.default;
 
   return (
-    <DocsPage toc={page.data.exports.toc} full={page.data.full}>
+    <DocsPage full={page.data.full} toc={page.data.exports.toc}>
       <DocsBody>
         <h1 className='mb-0'>{page.data.title}</h1>
-        <p className='mt-2'>{page.data.description}</p>
+        <p className='my-2'>{page.data.description}</p>
+
+        <div className='not-prose mb-12 space-x-2'>
+          {page.data.links?.doc && (
+            <a
+              href={page.data.links.doc}
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              <Badge>
+                Docs <ExternalLinkIcon className='ml-1.5 size-3' />
+              </Badge>
+            </a>
+          )}
+          {page.data.links?.api && (
+            <a
+              href={page.data.links.api}
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              <Badge>
+                API Reference <ExternalLinkIcon className='ml-1.5 size-3' />
+              </Badge>
+            </a>
+          )}
+        </div>
         <MDX />
       </DocsBody>
     </DocsPage>
   );
-}
+};
 
-export async function generateStaticParams() {
-  return getPages().map((page) => ({
-    slug: page.slugs,
-  }));
-}
-
-export function generateMetadata({ params }: { params: { slug?: string[] } }) {
-  const page = getPage(params.slug);
-
-  if (page == null) notFound();
-
-  return {
-    title: page.data.title,
-    description: page.data.description,
-  } satisfies Metadata;
-}
+export default Page;
