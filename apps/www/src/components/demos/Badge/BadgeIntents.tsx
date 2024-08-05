ceba859c3ca4cb29f057ef9e9b3c2a2f891bb050
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 16:44:47 -0600

    feat(components): create badge intents demo

---
+++ apps/www/src/components/demos/Badge/BadgeIntents.tsx
@@ -0,0 +1,13 @@
+import { Badge } from '@kosori/ui/badge';
+
+export const BadgeIntentsDemo = () => {
+  return (
+    <div className='space-x-4'>
+      <Badge>Default</Badge>
+      <Badge intent='info'>Info</Badge>
+      <Badge intent='success'>Success</Badge>
+      <Badge intent='warning'>Warning</Badge>
+      <Badge intent='danger'>Danger</Badge>
+    </div>
+  );
+};
