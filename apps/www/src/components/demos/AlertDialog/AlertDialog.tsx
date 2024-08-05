5b8d9772a5ff6726d975c56ec3abdd7f248b17fb
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 14:07:57 -0600

    feat(components): create alert dialog demo

---
+++ apps/www/src/components/demos/AlertDialog/AlertDialog.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@kosori/ui/alert-dialog';
+import { Button } from '@kosori/ui/button';
+
+export const AlertDialogDemo = () => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant='outline'>Delete account</Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction intent='danger'>
+            Yes, delete account
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
