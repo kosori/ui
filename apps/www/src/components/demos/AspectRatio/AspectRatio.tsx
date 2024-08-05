22bbecd8161f1b0b5082dfe21338a566ea52bcb9
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 15:07:43 -0600

    feat(components): create aspect ratio demo

---
+++ apps/www/src/components/demos/AspectRatio/AspectRatio.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+import { AspectRatio } from '@kosori/ui/aspect-ratio';
+
+export const AspectRatioDemo = () => {
+  return (
+    <div className='w-[384px] overflow-hidden rounded-xl'>
+      <AspectRatio ratio={16 / 9}>
+        <Image
+          alt='Landscape photograph by Tobias Tullius'
+          className='h-full w-full object-cover'
+          height={216}
+          src='https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80'
+          width={384}
+        />
+      </AspectRatio>
+    </div>
+  );
+};
