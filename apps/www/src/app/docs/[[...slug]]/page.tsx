c111b7b48a4171b1de0e3008b212b97996115d09
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 13:53:04 -0600

    feat(pages): hide div container of badges if there are not links

--- apps/www/src/app/docs/[[...slug]]/page.tsx
+++ apps/www/src/app/docs/[[...slug]]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import { ExternalLinkIcon } from '@radix-ui/react-icons';
+import { clsx } from 'clsx';
 import { DocsBody, DocsPage } from 'fumadocs-ui/page';
 
 import { Badge } from '@kosori/ui/badge';
@@ -41,32 +42,36 @@ const Page = ({ params }: { params: { slug?: string[] } }) => {
     <DocsPage full={page.data.full} toc={page.data.exports.toc}>
       <DocsBody>
         <h1 className='mb-0'>{page.data.title}</h1>
-        <p className='my-2'>{page.data.description}</p>
+        <p className={page.data.links ? 'my-2' : 'mb-12 mt-2'}>
+          {page.data.description}
+        </p>
 
-        <div className='not-prose mb-12 space-x-2'>
-          {page.data.links?.doc && (
-            <a
-              href={page.data.links.doc}
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              <Badge>
-                Docs <ExternalLinkIcon className='ml-1.5 size-3' />
-              </Badge>
-            </a>
-          )}
-          {page.data.links?.api && (
-            <a
-              href={page.data.links.api}
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              <Badge>
-                API Reference <ExternalLinkIcon className='ml-1.5 size-3' />
-              </Badge>
-            </a>
-          )}
-        </div>
+        {page.data.links && (
+          <div className='not-prose mb-12 space-x-2'>
+            {page.data.links.doc && (
+              <a
+                href={page.data.links.doc}
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                <Badge>
+                  Docs <ExternalLinkIcon className='ml-1.5 size-3' />
+                </Badge>
+              </a>
+            )}
+            {page.data.links.api && (
+              <a
+                href={page.data.links.api}
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                <Badge>
+                  API Reference <ExternalLinkIcon className='ml-1.5 size-3' />
+                </Badge>
+              </a>
+            )}
+          </div>
+        )}
         <MDX />
       </DocsBody>
     </DocsPage>
