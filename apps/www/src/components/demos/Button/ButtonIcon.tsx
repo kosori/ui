df6f37bb1ed3e003bb9cb69aadb78abcf3d3e541
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 17:47:54 -0600

    feat(components): create button icon demo

---
+++ apps/www/src/components/demos/Button/ButtonIcon.tsx
@@ -0,0 +1,19 @@
+import { Loader2 } from 'lucide-react';
+
+import { Button } from '@kosori/ui/Button';
+
+export const ButtonIconDemo = () => {
+  return (
+    <div className='flex gap-2'>
+      <Button icon size='small'>
+        <Loader2 />
+      </Button>
+      <Button icon>
+        <Loader2 />
+      </Button>
+      <Button icon size='large'>
+        <Loader2 />
+      </Button>
+    </div>
+  );
+};
