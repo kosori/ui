c4ab7e02a27b7ac9a7366303a8ac0ac86dd04115
Alexis Guzman <codingcodax@gmail.com>
2024-08-05 13:40:51 -0600

    feat(components): create alert error

---
+++ apps/www/src/components/demos/Alert/AlertError.tsx
@@ -0,0 +1,16 @@
+import { XCircle } from 'lucide-react';
+
+import { Alert, AlertDescription, AlertTitle } from '@kosori/ui/alert';
+
+export const AlertErrorDemo = () => {
+  return (
+    <Alert intent='error'>
+      <AlertTitle>
+        <XCircle /> Connection failed
+      </AlertTitle>
+      <AlertDescription>
+        Unable to establish connection with the server. Please try again later.
+      </AlertDescription>
+    </Alert>
+  );
+};
