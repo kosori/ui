66fa0081cf77af4f1466ec16e130a562782acc50
Alexis Guzman <codingcodax@gmail.com>
2024-08-06 17:39:02 -0600

    feat(components): export all carousel demos

---
+++ apps/www/src/components/demos/Carousel/index.ts
@@ -0,0 +1,6 @@
+export * from './Carousel';
+export * from './CarouselAPI';
+export * from './CarouselAutoplay';
+export * from './CarouselOrientation';
+export * from './CarouselSizes';
+export * from './CarouselSpacing';
