47ff7f0f3e93c388782bc0ffef9721247e17280d
Alexis Guzman <codingcodax@gmail.com>
2024-08-07 12:18:59 -0600

    feat(components): create checkbox form complex demo

---
+++ apps/www/src/components/demos/Checkbox/CheckboxFormComplex.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@kosori/ui/button';
+import { Checkbox } from '@kosori/ui/checkbox';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@kosori/ui/form';
+import { toast } from '@kosori/ui/toast';
+
+const items = [
+  {
+    id: 'recents',
+    label: 'Recents',
+  },
+  {
+    id: 'home',
+    label: 'Home',
+  },
+  {
+    id: 'applications',
+    label: 'Applications',
+  },
+  {
+    id: 'desktop',
+    label: 'Desktop',
+  },
+  {
+    id: 'downloads',
+    label: 'Downloads',
+  },
+  {
+    id: 'documents',
+    label: 'Documents',
+  },
+] as const;
+
+const FormSchema = z.object({
+  items: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: 'You have to select at least one item.',
+  }),
+});
+
+export const CheckboxFormComplexDemo = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      items: ['recents', 'home'],
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <div className='[&_figure]:mb-0 [&_figure]:mt-1'>
+          <CodeBlock allowCopy={false} lang='json'>
+            <Pre>{JSON.stringify(data, null, 2)}</Pre>
+          </CodeBlock>
+        </div>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form className='space-y-8' onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name='items'
+          render={() => (
+            <FormItem>
+              <div className='mb-4'>
+                <FormLabel className='text-base'>Sidebar</FormLabel>
+                <FormDescription>
+                  Select the items you want to display in the sidebar.
+                </FormDescription>
+              </div>
+              {items.map((item) => (
+                <FormField
+                  key={item.id}
+                  control={form.control}
+                  name='items'
+                  render={({ field }) => {
+                    return (
+                      <FormItem
+                        key={item.id}
+                        className='flex flex-row items-start space-x-3 space-y-0'
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value.includes(item.id)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.onChange([...field.value, item.id])
+                                : field.onChange(
+                                  field.value.filter(
+                                    (value) => value !== item.id,
+                                  ),
+                                );
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className='text-sm font-normal'>
+                          {item.label}
+                        </FormLabel>
+                      </FormItem>
+                    );
+                  }}
+                />
+              ))}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+};
