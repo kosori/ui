fc2449ab859068f3e0b351614fb53307bbc4dae8
Alexis Guzman <codingcodax@gmail.com>
2024-08-07 13:15:14 -0600

    feat(components): create combobox responsive demo

---
+++ apps/www/src/components/demos/Combobox/ComboboxResponsive.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Button } from '@kosori/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@kosori/ui/command';
+import { Drawer, DrawerContent, DrawerTrigger } from '@kosori/ui/drawer';
+import { Popover, PopoverContent, PopoverTrigger } from '@kosori/ui/popover';
+
+import { useMediaQuery } from '~/hooks/use-media-query';
+
+type Status = {
+  value: string;
+  label: string;
+};
+
+const statuses: Status[] = [
+  {
+    value: 'backlog',
+    label: 'Backlog',
+  },
+  {
+    value: 'todo',
+    label: 'Todo',
+  },
+  {
+    value: 'in progress',
+    label: 'In Progress',
+  },
+  {
+    value: 'done',
+    label: 'Done',
+  },
+  {
+    value: 'canceled',
+    label: 'Canceled',
+  },
+];
+
+export const ComboboxResponsiveDemo = () => {
+  const [open, setOpen] = useState(false);
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+  const [selectedStatus, setSelectedStatus] = useState<Status | null>(null);
+
+  if (isDesktop) {
+    return (
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button className='w-[150px] justify-start' variant='outline'>
+            {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent align='start' className='w-[200px] p-0'>
+          <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+        </PopoverContent>
+      </Popover>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button className='w-[150px] justify-start' variant='outline'>
+          {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className='mt-4 border-t'>
+          <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+const StatusList = ({
+  setOpen,
+  setSelectedStatus,
+}: {
+  setOpen: (open: boolean) => void;
+  setSelectedStatus: (status: Status | null) => void;
+}) => {
+  return (
+    <Command>
+      <CommandInput placeholder='Filter status...' />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup>
+          {statuses.map((status) => (
+            <CommandItem
+              key={status.value}
+              value={status.value}
+              onSelect={(value) => {
+                setSelectedStatus(
+                  statuses.find((priority) => priority.value === value) ?? null,
+                );
+                setOpen(false);
+              }}
+            >
+              {status.label}
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+};
