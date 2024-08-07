97b1b8e4495b15f897e7af4b9be4def38f089920
Alexis Guzman <codingcodax@gmail.com>
2024-08-07 13:09:43 -0600

    feat(ui): create collapsible component

---
+++ packages/ui/src/collapsible.tsx
@@ -0,0 +1,10 @@
+import { Content, Root, Trigger } from '@radix-ui/react-collapsible';
+
+// --- Component:Collapsible ---
+export const Collapsible = Root;
+
+// --- Component:CollapsibleContent ---
+export const CollapsibleContent = Content;
+
+// --- Component:CollapsibleTrigger ---
+export const CollapsibleTrigger = Trigger;
