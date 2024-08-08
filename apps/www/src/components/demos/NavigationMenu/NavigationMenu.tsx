4ae170244cc953e23d19591853b5488696bfea6f
Alexis Guzman <codingcodax@gmail.com>
2024-08-08 17:22:16 -0600

    feat(components): create navigation menu demo

---
+++ apps/www/src/components/demos/NavigationMenu/NavigationMenu.tsx
@@ -0,0 +1,163 @@
+'use client';
+
+import { forwardRef } from 'react';
+import Link from 'next/link';
+
+import { cn } from '@kosori/ui';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@kosori/ui/navigation-menu';
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Alert Dialog',
+    href: '/docs/primitives/alert-dialog',
+    description:
+      'A modal dialog that interrupts the user with important content and expects a response.',
+  },
+  {
+    title: 'Hover Card',
+    href: '/docs/primitives/hover-card',
+    description:
+      'For sighted users to preview content available behind a link.',
+  },
+  {
+    title: 'Progress',
+    href: '/docs/primitives/progress',
+    description:
+      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+  },
+  {
+    title: 'Scroll-area',
+    href: '/docs/primitives/scroll-area',
+    description: 'Visually or semantically separates content.',
+  },
+  {
+    title: 'Tabs',
+    href: '/docs/primitives/tabs',
+    description:
+      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
+  },
+  {
+    title: 'Tooltip',
+    href: '/docs/primitives/tooltip',
+    description:
+      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+  },
+];
+
+export const NavigationMenuDemo = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul
+              className={cn(
+                'grid gap-3 p-4',
+                'md:w-[400px]',
+                'lg:w-[500px] lg:grid-cols-[.75fr_1fr]',
+              )}
+            >
+              <li className='row-span-3'>
+                <NavigationMenuLink asChild>
+                  <a
+                    className={cn(
+                      'flex h-full w-full select-none flex-col justify-end rounded-lg bg-gradient-to-b from-grey-line/50 to-grey-bg-subtle p-6 no-underline outline-none',
+                      'focus:shadow-md',
+                    )}
+                    href='/'
+                  >
+                    <div className='h-6 w-6 rounded-md bg-grey-text-contrast' />
+                    <div className='mb-2 mt-4 text-lg font-medium'>
+                      kosori/ui
+                    </div>
+                    <p className='text-sm leading-tight text-grey-text'>
+                      Beautifully designed components built with Radix UI and
+                      Tailwind CSS.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href='/docs' title='Introduction'>
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href='/docs/installation' title='Installation'>
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href='/docs/primitives/typography' title='Typography'>
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul
+              className={cn(
+                'grid w-[400px] gap-3 p-4',
+                'md:w-[500px] md:grid-cols-2',
+                'lg:w-[600px]',
+              )}
+            >
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  href={component.href}
+                  title={component.title}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link legacyBehavior passHref href='/docs'>
+            <NavigationMenuLink className={navigationMenuTriggerStyle}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+const ListItem = forwardRef<
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-lg p-3 leading-none no-underline outline-none transition-colors',
+            'hover:bg-primary-bg-hover',
+            'focus:bg-primary-bg-hover',
+            'active:bg-primary-bg-active',
+            className,
+          )}
+          {...props}
+        >
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='line-clamp-2 text-sm leading-snug text-grey-text'>
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+
+ListItem.displayName = 'ListItem';
