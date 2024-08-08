716a27e1857db4f962c18068c324bf89040186b4
Alexis Guzman <codingcodax@gmail.com>
2024-08-08 09:04:50 -0600

    feat(components): create dropdown menu checkboxes demo

---
+++ apps/www/src/components/demos/DropdownMenu/DropdownMenuCheckboxes.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import type { DropdownMenuCheckboxItemProps } from '@radix-ui/react-dropdown-menu';
+import { useState } from 'react';
+
+import { Button } from '@kosori/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@kosori/ui/dropdown-menu';
+
+type Checked = DropdownMenuCheckboxItemProps['checked'];
+
+export const DropdownMenuCheckboxesDemo = () => {
+  const [showStatusBar, setShowStatusBar] = useState<Checked>(true);
+  const [showActivityBar, setShowActivityBar] = useState<Checked>(false);
+  const [showPanel, setShowPanel] = useState<Checked>(false);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline'>Open</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56'>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={showStatusBar}
+          onCheckedChange={setShowStatusBar}
+        >
+          Status Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          disabled
+          checked={showActivityBar}
+          onCheckedChange={setShowActivityBar}
+        >
+          Activity Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showPanel}
+          onCheckedChange={setShowPanel}
+        >
+          Panel
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
