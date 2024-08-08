39f90a73c15ee310e7e1029b947c4075667bb106
Alexis Guzman <codingcodax@gmail.com>
2024-08-07 18:29:02 -0600

    feat(components): create drawer dialog demo

---
+++ apps/www/src/components/demos/Drawer/DrawerDialog.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { useState } from 'react';
+
+import { cn } from '@kosori/ui';
+import { Button } from '@kosori/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@kosori/ui/dialog';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@kosori/ui/drawer';
+import { Input } from '@kosori/ui/input';
+import { Label } from '@kosori/ui/label';
+
+import { useMediaQuery } from '~/hooks/use-media-query';
+
+export const DrawerDialogDemo = () => {
+  const [open, setOpen] = useState(false);
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant='outline'>Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className='sm:max-w-[425px]'>
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <ProfileForm />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant='outline'>Edit Profile</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className='text-left'>
+          <DrawerTitle>Edit profile</DrawerTitle>
+          <DrawerDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DrawerDescription>
+        </DrawerHeader>
+        <ProfileForm className='px-4' />
+        <DrawerFooter className='pt-2'>
+          <DrawerClose asChild>
+            <Button className='w-full' variant='outline'>
+              Cancel
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+const ProfileForm = ({ className }: React.ComponentProps<'form'>) => {
+  return (
+    <form className={cn('grid items-start gap-4', className)}>
+      <div className='grid gap-2'>
+        <Label htmlFor='email'>Email</Label>
+        <Input defaultValue='shadcn@example.com' id='email' type='email' />
+      </div>
+      <div className='grid gap-2'>
+        <Label htmlFor='username'>Username</Label>
+        <Input defaultValue='@shadcn' id='username' />
+      </div>
+      <Button className='w-full' type='submit'>
+        Save changes
+      </Button>
+    </form>
+  );
+};
