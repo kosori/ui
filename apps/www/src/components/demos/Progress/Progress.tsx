f75a36fe52f7641502c2800ef77d0691a3350b0e
Alexis Guzman <codingcodax@gmail.com>
2024-08-08 18:16:33 -0600

    feat(components): create progress demo

---
+++ apps/www/src/components/demos/Progress/Progress.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { Progress } from '@kosori/ui/progress';
+
+export const ProgressDemo = () => {
+  const [progress, setProgress] = useState(13);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <Progress className='w-[60%]' value={progress} />;
+};
