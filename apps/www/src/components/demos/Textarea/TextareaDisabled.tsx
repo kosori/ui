01c118fc87b1b0fd960c61a1288024453265262d
Alexis Guzman <codingcodax@gmail.com>
2024-08-09 17:40:18 -0600

    feat(components): create textarea disabled demo

---
+++ apps/www/src/components/demos/Textarea/TextareaDisabled.tsx
@@ -0,0 +1,11 @@
+import { Textarea } from '@kosori/ui/textarea';
+
+export const TextareaDisabledDemo = () => {
+  return (
+    <Textarea
+      disabled
+      className='max-w-md'
+      placeholder='Type your message here.'
+    />
+  );
+};
