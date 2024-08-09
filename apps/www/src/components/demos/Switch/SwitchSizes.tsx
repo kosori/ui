993b9b48d0f95275a9970ce201540e2831625206
Alexis Guzman <codingcodax@gmail.com>
2024-08-09 14:30:07 -0600

    feat(components): create switch sizes demo

---
+++ apps/www/src/components/demos/Switch/SwitchSizes.tsx
@@ -0,0 +1,23 @@
+import { Label } from '@kosori/ui/label';
+import { Switch } from '@kosori/ui/switch';
+
+export const SwitchSizesDemo = () => {
+  return (
+    <div className='space-x-4'>
+      <div className='inline-flex items-center gap-x-2'>
+        <Switch id='small' size='small' />
+        <Label htmlFor='small'>Small</Label>
+      </div>
+
+      <div className='inline-flex items-center gap-x-2'>
+        <Switch id='medium' size='medium' />
+        <Label htmlFor='medium'>Medium</Label>
+      </div>
+
+      <div className='inline-flex items-center gap-x-2'>
+        <Switch id='big' size='big' />
+        <Label htmlFor='big'>Big</Label>
+      </div>
+    </div>
+  );
+};
