839294bbf190dd12e6a2c6e1b2cb93d10d101e9f
Alexis Guzman <codingcodax@gmail.com>
2024-08-09 17:41:00 -0600

    feat(components): export all textarea demos

---
+++ apps/www/src/components/demos/Textarea/index.ts
@@ -0,0 +1,6 @@
+export * from './Textarea';
+export * from './TextareaButton';
+export * from './TextareaDisabled';
+export * from './TextareaForm';
+export * from './TextareaLabel';
+export * from './TextareaText';
