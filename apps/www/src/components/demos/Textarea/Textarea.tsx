aab0d0ec5dceae994032f363b24bdc2fc8f8813f
Alexis Guzman <codingcodax@gmail.com>
2024-08-09 17:39:39 -0600

    feat(components): create textarea demo

---
+++ apps/www/src/components/demos/Textarea/Textarea.tsx
@@ -0,0 +1,7 @@
+import { Textarea } from '@kosori/ui/textarea';
+
+export const TextareaDemo = () => {
+  return (
+    <Textarea className='max-w-md' placeholder='Type your message here.' />
+  );
+};
