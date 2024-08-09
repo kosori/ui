b7bd64fdc1c664e61a2a8c7772799c73bd498144
Alexis Guzman <codingcodax@gmail.com>
2024-08-09 13:15:35 -0600

    feat(components): create sonner demo

---
+++ apps/www/src/components/demos/Sonner/Sonner.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { toast } from 'sonner';
+
+import { Button } from '@kosori/ui/button';
+
+export const SonnerDemo = () => {
+  return (
+    <Button
+      variant='outline'
+      onClick={() =>
+        toast('Event has been created', {
+          description: 'Sunday, December 03, 2023 at 9:00 AM',
+          action: {
+            label: 'Undo',
+            onClick: () => console.log('Undo'),
+          },
+          cancel: {
+            label: 'Delete',
+            onClick: () => console.log('Delete'),
+          },
+          duration: 10000,
+        })
+      }
+    >
+      Show Toast
+    </Button>
+  );
+};
