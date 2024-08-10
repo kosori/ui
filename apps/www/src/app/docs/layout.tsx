e5099567f1c3e9e8113e62b56e51dcc56ef2458c
Alexis Guzman <codingcodax@gmail.com>
2024-08-10 17:17:55 -0600

    fix(pages): update docs options import path

--- apps/www/src/app/docs/layout.tsx
+++ apps/www/src/app/docs/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 import { DocsLayout } from 'fumadocs-ui/layout';
 
-import { docsOptions } from '../layout.config';
+import { docsOptions } from '~/config/layout';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
