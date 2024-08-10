45ace98e46efd7bc42220277a2dc230a1ec69814
Alexis Guzman <codingcodax@gmail.com>
2024-08-10 09:50:57 -0600

    feat(components): export all toggle group demos

---
+++ apps/www/src/components/demos/ToggleGroup/index.ts
@@ -0,0 +1,5 @@
+export * from './ToggleGroup';
+export * from './ToggleGroupDisabled';
+export * from './ToggleGroupSingle';
+export * from './ToggleGroupSizes';
+export * from './ToggleGroupVariants';
