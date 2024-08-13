4f9959cd8676e4d04b8cdd1b72fd9b67fe023c49
Alexis Guzman <codingcodax@gmail.com>
2024-08-13 13:34:16 -0600

    feat(components): add accordion and accordions

--- apps/www/src/mdx-components.tsx
+++ apps/www/src/mdx-components.tsx
@@ -1,4 +1,5 @@
 import type { MDXComponents } from 'mdx/types';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
 import { Step, Steps } from 'fumadocs-ui/components/steps';
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import defaultComponents from 'fumadocs-ui/mdx';
@@ -9,6 +10,8 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    Accordions,
+    Accordion,
     Steps,
     Step,
     Tab,
