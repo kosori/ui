ec01ace57174474d341ae2c241ed22f8c5037e6e
Alexis Guzman <codingcodax@gmail.com>
2024-08-15 12:55:35 -0600

    feat(pages): create `/theme-builder`

---
+++ apps/www/src/app/(marketing)/theme-builder/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const ThemeBuilder: NextPage = () => {
+  return (
+    <div>
+      <p>theme builder page</p>
+    </div>
+  );
+};
+
+export default ThemeBuilder;
