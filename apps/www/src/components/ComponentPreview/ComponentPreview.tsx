f2ddfb60916295c59c0fae924a38ab77e9978600
Alexis Guzman <codingcodax@gmail.com>
2024-08-15 14:18:27 -0600

    feat(components): set border radius to `xl`

--- apps/www/src/components/ComponentPreview/ComponentPreview.tsx
+++ apps/www/src/components/ComponentPreview/ComponentPreview.tsx
@@ -16,7 +16,7 @@ export const ComponentPreview = ({ name, children }: Props) => {
           <code className='relative rounded bg-grey-bg px-[0.3rem] py-[0.2rem] font-mono text-sm'>
             {name}
           </code>{' '}
-          not found in registry.
+          not found in the library.
         </p>
       );
     }
@@ -26,7 +26,10 @@ export const ComponentPreview = ({ name, children }: Props) => {
 
   return (
     <div>
-      <Tabs className='overflow-visible' items={['Preview', 'Code']}>
+      <Tabs
+        className='overflow-visible [&>*:first-child]:rounded-t-xl'
+        items={['Preview', 'Code']}
+      >
         <Tab className='p-0' value='Preview'>
           <div className='not-prose flex min-h-[350px] items-center justify-center p-4'>
             {PreviewComponent}
