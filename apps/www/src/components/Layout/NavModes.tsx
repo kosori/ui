6d0d01e40d249b5ae0b111a906650c11ef91d27d
Alexis Guzman <codingcodax@gmail.com>
2024-08-15 14:18:09 -0600

    feat(components): increase border radius to `lg`

--- apps/www/src/components/Layout/NavModes.tsx
+++ apps/www/src/components/Layout/NavModes.tsx
@@ -5,12 +5,12 @@ import { modes } from '~/config/modes';
 
 export const NavModes = () => {
   return (
-    <div className='rounded-md border p-1 text-sm text-grey-text max-md:absolute max-md:left-1/2 max-md:-translate-x-1/2'>
+    <div className='rounded-lg border p-1 text-sm text-grey-text max-md:absolute max-md:left-1/2 max-md:-translate-x-1/2'>
       {modes.map((m) => (
         <Link
           key={m.param}
           className={clsx(
-            'rounded-md px-2 py-1 text-grey-text transition-colors',
+            'px-2 py-1 font-medium text-grey-text transition-colors',
             'hover:text-grey-text-contrast',
           )}
           href={`/docs/${m.param}`}
