d0968d1ac6db1038765ddd68c3e234fa5febbcba
Alexis Guzman <codingcodax@gmail.com>
2024-08-22 18:38:50 -0600

    feat(components): create date picker form demo

---
+++ apps/www/src/components/demos/DatePicker/DatePickerForm.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { format } from 'date-fns';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { cn } from '@kosori/ui';
+import { Button } from '@kosori/ui/button';
+import { Calendar } from '@kosori/ui/calendar';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@kosori/ui/form';
+import { Popover, PopoverContent, PopoverTrigger } from '@kosori/ui/popover';
+import { toast } from '@kosori/ui/toast';
+
+const FormSchema = z.object({
+  dob: z.date({
+    required_error: 'A date of birth is required.',
+  }),
+});
+
+export const DatePickerFormDemo = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+          <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form className='space-y-8' onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name='dob'
+          render={({ field }) => (
+            <FormItem className='flex flex-col'>
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      className={cn(
+                        'w-[248.8px] pl-3 text-left font-normal',
+                        !field.value && 'text-muted-foreground',
+                      )}
+                      variant={'outline'}
+                    >
+                      {field.value ? (
+                        format(field.value, 'PPP')
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent align='start' className='w-auto p-0'>
+                  <Calendar
+                    disabled={(date) =>
+                      date > new Date() || date < new Date('1900-01-01')
+                    }
+                    mode='single'
+                    selected={field.value}
+                    onSelect={field.onChange}
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+};
