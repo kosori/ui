f82933e706393980ed752c7581e87fe44c6c47dd
Alexis Guzman <codingcodax@gmail.com>
2024-08-22 18:38:43 -0600

    feat(components): create date picker demo

---
+++ apps/www/src/components/demos/DatePicker/DatePicker.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState } from 'react';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { clsx } from 'clsx/lite';
+import { format } from 'date-fns';
+
+import { Button } from '@kosori/ui/button';
+import { Calendar } from '@kosori/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@kosori/ui/popover';
+
+export const DatePickerDemo = () => {
+  const [date, setDate] = useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          className={clsx(
+            'w-[240px] justify-start text-left font-normal',
+            !date && 'text-grey-text',
+          )}
+          variant={'outline'}
+        >
+          <CalendarIcon />
+
+          {date ? format(date, 'PPP') : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent align='start' className='w-auto p-0'>
+        <Calendar mode='single' selected={date} onSelect={setDate} />
+      </PopoverContent>
+    </Popover>
+  );
+};
