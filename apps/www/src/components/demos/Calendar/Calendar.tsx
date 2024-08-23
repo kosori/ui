fb267cbd77eab1edf1ee5b7c30a142171543e02f
Alexis Guzman <codingcodax@gmail.com>
2024-08-22 18:38:36 -0600

    fix(components): change to `mode` `single`

--- apps/www/src/components/demos/Calendar/Calendar.tsx
+++ apps/www/src/components/demos/Calendar/Calendar.tsx
@@ -1,18 +1,16 @@
 'use client';
 
-import type { DateRange } from 'react-day-picker';
 import { useState } from 'react';
 
 import { Calendar } from '@kosori/ui/calendar';
 
 export const CalendarDemo = () => {
-  const [selected, setSelected] = useState<DateRange | undefined>();
+  const [selected, setSelected] = useState<Date | undefined>();
 
   return (
     <Calendar
-      className='rounded-lg border shadow'
-      disabled={[new Date('2024-07-01'), new Date('2024-06-10')]}
-      mode='range'
+      className='rounded-xl border shadow'
+      mode='single'
       selected={selected}
       onSelect={setSelected}
     />
