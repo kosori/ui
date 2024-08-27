32bcadb062f88bd3bd4e21b70e969a2a3ef91d14
Alexis Guzman <codingcodax@gmail.com>
2024-08-27 17:33:31 -0600

    feat(hooks): update use theme config

--- apps/www/src/hooks/use-theme-config.ts
+++ apps/www/src/hooks/use-theme-config.ts
@@ -1,29 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useAtom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 
 import { defaultConfig } from '~/config/theme';
 
 export type ConfigKey = keyof typeof defaultConfig;
 type UpdateColor = { key: ConfigKey; value: string };
 
+const configAtom = atomWithStorage('themeConfig', defaultConfig);
+
 export const useThemeConfig = () => {
-  const [config, setConfig] = useState(defaultConfig);
+  const [config, setConfig] = useAtom(configAtom);
 
-  useEffect(() => {
-    const storedColors = localStorage.getItem('themeConfig');
-
-    if (storedColors) {
-      setConfig(JSON.parse(storedColors) as typeof defaultConfig);
-    }
-  }, []);
-
-  const updateConfig = ({ key, value }: UpdateColor) => {
-    const newConfig = { ...config, [key]: value };
-    const themeWrapper = document.querySelector(`body[data-${key}]`);
-
-    setConfig(newConfig);
-    if (themeWrapper) themeWrapper.setAttribute(`data-${key}`, value);
-    localStorage.setItem('themeConfig', JSON.stringify(newConfig));
-  };
+  const updateConfig = ({ key, value }: UpdateColor) =>
+    setConfig({ ...config, [key]: value });
 
   return { config, updateConfig };
 };
