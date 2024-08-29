37186053cc347ae9ff613c24aa4853594bfe98b4
Alexis Guzman <codingcodax@gmail.com>
2024-08-29 09:03:10 -0600

    feat(components): aling popover content

--- apps/www/src/components/Layout/BorderRadius.tsx
+++ apps/www/src/components/Layout/BorderRadius.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { CornersIcon } from '@radix-ui/react-icons';
 import { clsx } from 'clsx/lite';
+import { BoxSelectIcon } from 'lucide-react';
 
 import { Button } from '@kosori/ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@kosori/ui/popover';
+import { Separator } from '@kosori/ui/separator';
 
 import { borderRadius } from '~/config/theme';
 import { useThemeConfig } from '~/hooks/use-theme-config';
@@ -20,38 +21,53 @@ export const BorderRadius = () => {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button icon className='min-w-9' variant='ghost'>
-          <CornersIcon />
+        <Button
+          icon
+          className={clsx('min-w-9', 'data-[state=open]:bg-grey-bg-active')}
+          variant='ghost'
+        >
+          <BoxSelectIcon />
           <span className='sr-only'>{selectedBorderRadius}</span>
         </Button>
       </PopoverTrigger>
 
       <PopoverContent
-        className='grid w-80 grid-cols-3 gap-1'
+        className={clsx(
+          'w-96 translate-x-11',
+          'data-[state=closed]:slide-out-to-right-11',
+          'data-[state=open]:slide-in-from-right-11',
+        )}
         side='top'
         sideOffset={14}
       >
-        {borderRadius.map(({ name, value, size }) => (
-          <Button
-            key={name}
-            className={clsx(
-              'w-full justify-start rounded-none',
-              config.radius === value && 'bg-grey-bg hover:bg-grey-bg',
-            )}
-            size='small'
-            style={{ borderRadius: size }}
-            variant='outline'
-            onClick={() => handleClick({ value })}
-          >
-            <div className='size-3.5 overflow-hidden'>
-              <span
-                className='inline-block size-6 translate-x-[20%] translate-y-[20%] border border-grey-text-contrast'
-                style={{ borderRadius: size }}
-              />
-            </div>
-            <span className='truncate'>{name}</span>
-          </Button>
-        ))}
+        <h3 className='font-medium'>Border Radius</h3>
+        <p className='text-sm text-grey-text'>Playful or professional?</p>
+
+        <Separator className='my-2' />
+
+        <div className='grid grid-cols-3 gap-1'>
+          {borderRadius.map(({ name, value, size }) => (
+            <Button
+              key={name}
+              className={clsx(
+                'w-full justify-start rounded-none',
+                config.radius === value && 'bg-grey-bg hover:bg-grey-bg',
+              )}
+              size='small'
+              style={{ borderRadius: size }}
+              variant='outline'
+              onClick={() => handleClick({ value })}
+            >
+              <div className='size-3.5 overflow-hidden'>
+                <span
+                  className='inline-block size-6 translate-x-[20%] translate-y-[20%] border border-grey-text-contrast'
+                  style={{ borderRadius: size }}
+                />
+              </div>
+              <span className='truncate'>{name}</span>
+            </Button>
+          ))}
+        </div>
       </PopoverContent>
     </Popover>
   );
