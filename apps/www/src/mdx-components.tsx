82e4faaecf98e140433fa8b0d31099af913d11ae
Alexis Guzman <codingcodax@gmail.com>
2024-08-30 10:37:33 -0600

    feat(components): setup default background color to codeblock

--- apps/www/src/mdx-components.tsx
+++ apps/www/src/mdx-components.tsx
@@ -1,5 +1,6 @@
 import type { MDXComponents } from 'mdx/types';
 import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import { File, Files, Folder } from 'fumadocs-ui/components/files';
 import { Step, Steps } from 'fumadocs-ui/components/steps';
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
@@ -12,6 +13,11 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props} keepBackground className='bg-grey-base'>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     ComponentPreview,
     Accordion,
     Accordions,
