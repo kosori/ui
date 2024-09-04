a89d9a1b161e1da03439577c05ba3fea8655d764
Alexis Guzman <codingcodax@gmail.com>
2024-09-04 13:38:21 -0600

    feat(schema): create type, items and init options

---
+++ packages/cli/src/commands/add/schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+export const typeSchema = z
+  .literal('components')
+  .or(z.literal('hooks'))
+  .optional();
+
+export const itemsSchema = z.array(z.string());
+
+export const initOptionsSchema = z.object({
+  yes: z.boolean(),
+  overwrite: z.boolean(),
+  cwd: z.string(),
+  all: z.boolean(),
+  path: z.string(),
+  silent: z.boolean(),
+});
