5e36bece067c26ab0f22a3724650545dda537f16
Alexis Guzman <codingcodax@gmail.com>
2024-09-03 18:05:26 -0600

    feat: create main

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,23 @@
-export const name = 'cli';
+import { Command } from 'commander';
+
+import packageJson from '../package.json';
+import { add } from './commands/add';
+import { init } from './commands/init';
+
+const main = () => {
+  const program = new Command();
+  program
+    .name('kosori')
+    .description('Kõsori CLI')
+    .version(
+      packageJson.version || '1.0.0',
+      '-v, --version',
+      'display the current version',
+    );
+
+  program.addCommand(init).addCommand(add);
+
+  program.parse();
+};
+
+main();
