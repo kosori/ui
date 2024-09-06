6634896cb0444e36dde139d7db2f296fb16064e8
Alexis Guzman <codingcodax@gmail.com>
2024-09-06 08:15:46 -0600

    fix(scripts): update scripts import path

--- apps/www/src/scripts/writeUiJson.ts
+++ apps/www/src/scripts/writeUiJson.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { convertFilesToJson } from '~/config/scripts';
+import { convertFilesToJson } from '~/helpers/scripts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
