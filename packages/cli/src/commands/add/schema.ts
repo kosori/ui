dee5ae7ce939f509a37349d996d8959d4127a30e
Alexis Guzman <codingcodax@gmail.com>
2024-09-07 17:04:42 -0600

    feat(schema): rename schemas

--- packages/cli/src/commands/add/schema.ts
+++ packages/cli/src/commands/add/schema.ts
@@ -16,11 +16,15 @@ export const initOptionsSchema = z.object({
   silent: z.boolean(),
 });
 
-export const component = z.object({
+export const indexSingle = z.object({
   name: z.string(),
   dependencies: z.array(z.string()).optional(),
   required: z.array(z.string()).optional(),
   type: z.literal('component:ui'),
 });
+
+export const index = z.array(indexSingle);
+
+export const component = indexSingle.extend({ content: z.string() });
 
 export const components = z.array(component);
