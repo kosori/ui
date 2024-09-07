7ab22c57f551c6a2b1828a6845412bba0911964b
Alexis Guzman <codingcodax@gmail.com>
2024-09-07 16:22:23 -0600

    feat(prompts): get components

--- packages/cli/src/commands/add/prompts.ts
+++ packages/cli/src/commands/add/prompts.ts
@@ -1,11 +1,19 @@
 import * as p from '@clack/prompts';
 
 import { highlight } from '~/utils/highlight';
+import { getComponentsList } from './helpers/components';
+
+const components = await getComponentsList();
 
 export const componentsPrompts = {
-  componentsAlias: () =>
+  components: () =>
     p.multiselect({
       message: `Which ${highlight('components')} would you like to add?`,
-      options: [],
+      options: components.map(({ name, required }) => ({
+        value: name,
+        label: name,
+        hint: required?.length ? 'Will install another component(s)' : '',
+      })),
+      required: true,
     }),
 };
