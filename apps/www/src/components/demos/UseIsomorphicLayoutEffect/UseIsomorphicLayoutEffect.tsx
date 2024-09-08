be511f22d77fa47b70fe203cec3da608468a619e
Alexis Guzman <codingcodax@gmail.com>
2024-09-08 11:54:14 -0600

    feat(components): create use isomorphic layout effect demo

---
+++ apps/www/src/components/demos/UseIsomorphicLayoutEffect/UseIsomorphicLayoutEffect.tsx
@@ -0,0 +1,15 @@
+import { useIsomorphicLayoutEffect } from '~/hooks/use-isomorphic-layout-effect';
+
+export const UseIsomorphicLayoutEffectDemo = () => {
+  useIsomorphicLayoutEffect(() => {
+    console.log(
+      "In the browser, I'm an `useLayoutEffect`, but in SSR I'm an `useEffect`",
+    );
+  }, []);
+
+  return (
+    <div>
+      <p>Check the console</p>
+    </div>
+  );
+};
