ebfb3059e64ee576bdb2d0a79804baf79a63f90d
Alexis Guzman <codingcodax@gmail.com>
2024-09-08 11:46:06 -0600

    feat(components): create use media query demo

---
+++ apps/www/src/components/demos/UseMediaQuery/UseMediaQuery.tsx
@@ -0,0 +1,11 @@
+import { useMediaQuery } from '~/hooks/use-media-query';
+
+export const UseMediaQueryDemo = () => {
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+
+  return (
+    <div>
+      <p>The current viewport is {isDesktop ? 'desktop' : 'mobile'}</p>
+    </div>
+  );
+};
