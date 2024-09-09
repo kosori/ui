a15f0a03424f06cfdb733900e28c6fefd39bedde
Alexis Guzman <codingcodax@gmail.com>
2024-09-09 14:40:11 -0600

    feat(helpers): create get components function

--- packages/cli/src/commands/add/helpers/components.ts
+++ packages/cli/src/commands/add/helpers/components.ts
@@ -1,5 +1,3 @@
-import { existsSync, promises as fs } from 'fs';
-
 import { fetchJson } from '~/utils/fetchJson';
 import { component, index } from '../schema';
 
@@ -17,23 +15,12 @@ export const getComponent = async ({ name }: { name: string }) => {
   return component.parse(componentJson);
 };
 
-export const writeComponent = async ({
-  component,
-  cwd,
-  overwrite,
-}: {
-  component: string;
-  cwd: string;
-  overwrite: boolean;
-}) => {
-  const componentPath = `${cwd}/components/${component}.json`;
-  const exists = existsSync(componentPath);
+export const getComponents = async ({ names }: { names: string[] }) => {
+  const components = await Promise.all(
+    names.map(async (component) => {
+      return await getComponent({ name: component });
+    }),
+  );
 
-  if (exists && !overwrite) {
-    throw new Error(`Component ${component} already exists`);
-  }
-
-  await fs.writeFile(componentPath, JSON.stringify({}));
-
-  return true;
+  return components;
 };
