eb9ddd6f54387447cbddee043e97aba1177d9dbc
Alexis Guzman <codingcodax@gmail.com>
2024-09-12 12:41:04 -0600

    feat(source): move schema

--- apps/www/source.config.ts
+++ apps/www/source.config.ts
@@ -1,8 +1,22 @@
 import { rehypeCode, remarkImage } from 'fumadocs-core/mdx-plugins';
 import { fileGenerator, remarkDocGen, remarkInstall } from 'fumadocs-docgen';
-import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
+import {
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+} from 'fumadocs-mdx/config';
+import { z } from 'zod';
 
-export const { docs, meta } = defineDocs();
+export const { docs, meta } = defineDocs({
+  docs: {
+    schema: frontmatterSchema.extend({
+      links: z
+        .object({ doc: z.string().optional(), api: z.string().optional() })
+        .optional(),
+      dependencies: z.array(z.string()).optional(),
+    }),
+  },
+});
 
 export default defineConfig({
   mdxOptions: {
--- apps/www/src/app/source.ts
+++ apps/www/src/app/source.ts
@@ -1,16 +1,9 @@
 import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from 'fumadocs-mdx';
-import { z } from 'zod';
 
 import { docs, meta } from '../../.source';
 
 export const source = loader({
   baseUrl: '/docs',
   source: createMDXSource(docs, meta),
-  schema: z.object({
-    links: z
-      .object({ doc: z.string().optional(), api: z.string().optional() })
-      .optional(),
-    dependencies: z.array(z.string()).optional(),
-  }),
 });
