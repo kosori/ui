c218fa1c289df08cb9a2f745f99aedd2d8063c1d
Alexis Guzman <codingcodax@gmail.com>
2024-09-11 18:40:28 -0600

    fix(pages): add mdx components

--- apps/www/src/app/docs/[[...slug]]/page.tsx
+++ apps/www/src/app/docs/[[...slug]]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { ExternalLinkIcon } from '@radix-ui/react-icons';
 import { clsx } from 'clsx';
@@ -5,14 +6,13 @@ import { DocsBody, DocsPage } from 'fumadocs-ui/page';
 
 import { Badge } from '@kosori/ui/badge';
 
-import { docs } from '~/app/source';
+import { source } from '~/app/source';
+import { MDXComponents } from '~/components/Mdx';
 import { versions } from '~/config/versions';
 import { Contribute } from '../_components/Contribute';
 
 export const generateStaticParams = () => {
-  return docs.getPages().map((page) => ({
-    slug: page.slugs,
-  }));
+  return source.generateParams();
 };
 
 export const generateMetadata = ({
@@ -20,108 +20,35 @@ export const generateMetadata = ({
 }: {
   params: { slug?: string[] };
 }) => {
-  const page = docs.getPage(params.slug);
-
-  if (page == null) notFound();
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
 
   return {
-    title: `${page.data.title} - kōsori`,
+    title: page.data.title,
     description: page.data.description,
-    links: page.data.links,
-    dependencies: page.data.dependencies,
-  };
+  } satisfies Metadata;
 };
 
 const Page = ({ params }: { params: { slug?: string[] } }) => {
-  const page = docs.getPage(params.slug);
+  const page = source.getPage(params.slug);
 
   if (page == null) {
     notFound();
   }
 
   const path = `apps/www/content/docs/${page.file.path}`;
-  const MDX = page.data.exports.default;
+  const MDX = page.data.body;
 
   return (
     <DocsPage
       full={page.data.full}
       tableOfContent={{ footer: <Contribute path={path} /> }}
       tableOfContentPopover={{ footer: <Contribute path={path} /> }}
-      toc={page.data.exports.toc}
+      toc={page.data.toc}
     >
       <DocsBody>
         <h1 className='mb-0'>{page.data.title}</h1>
-        <p
-          className={
-            page.data.links || page.data.dependencies ? 'my-2' : 'mb-12 mt-2'
-          }
-        >
-          {page.data.description}
-        </p>
-
-        {page.data.links && (
-          <div
-            className={clsx(
-              'not-prose space-x-2',
-              page.data.dependencies ? 'mb-4' : 'mb-12',
-            )}
-          >
-            {page.data.links.doc && (
-              <a
-                href={page.data.links.doc}
-                rel='noopener noreferrer'
-                target='_blank'
-              >
-                <Badge>
-                  Docs <ExternalLinkIcon className='ml-1.5 size-3' />
-                </Badge>
-              </a>
-            )}
-            {page.data.links.api && (
-              <a
-                href={page.data.links.api}
-                rel='noopener noreferrer'
-                target='_blank'
-              >
-                <Badge>
-                  API Reference <ExternalLinkIcon className='ml-1.5 size-3' />
-                </Badge>
-              </a>
-            )}
-          </div>
-        )}
-
-        {page.data.dependencies && (
-          <div className='not-prose mb-12 space-x-2'>
-            <div
-              className={clsx(
-                'flex flex-col items-start gap-4 border-t pt-4',
-                'min-[480px]:flex-row min-[480px]:items-center',
-              )}
-            >
-              <span className='text-fd-muted-foreground text-xs'>
-                Dependencies
-              </span>
-
-              <div className='flex flex-wrap gap-2'>
-                {page.data.dependencies.map((dep) => (
-                  <a
-                    key={dep}
-                    href={`https://www.npmjs.com/package/${dep}/v/${versions[dep]}`}
-                    rel='noopener noreferrer'
-                    target='_blank'
-                  >
-                    <Badge size='small' variant='outline'>
-                      {dep}: v{versions[dep]}{' '}
-                      <ExternalLinkIcon className='ml-1.5 size-2.5' />
-                    </Badge>
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
-        <MDX />
+        <MDX components={MDXComponents} />
       </DocsBody>
     </DocsPage>
   );
