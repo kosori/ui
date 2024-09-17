de2b12575da36125770511479298859b6bb4038d
Alexis Guzman <codingcodax@gmail.com>
2024-09-16 18:55:48 -0600

    feat(components): update gap size and items position

--- apps/www/src/components/demos/Button/ButtonSizes.tsx
+++ apps/www/src/components/demos/Button/ButtonSizes.tsx
@@ -2,7 +2,7 @@ import { Button } from '@kosori/ui/button';
 
 export const ButtonSizesDemo = () => {
   return (
-    <div className='flex gap-2'>
+    <div className='flex items-end gap-4'>
       <Button size='small'>Small</Button>
       <Button>Medium</Button>
       <Button size='large'>Large</Button>
