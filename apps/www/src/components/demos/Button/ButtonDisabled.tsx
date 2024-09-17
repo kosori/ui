5691ae9a16556e1d6de680074291999bab4e0991
Alexis Guzman <codingcodax@gmail.com>
2024-09-17 11:24:22 -0600

    feat(components): setup button disabled demo

--- apps/www/src/components/demos/Button/ButtonDisabled.tsx
+++ apps/www/src/components/demos/Button/ButtonDisabled.tsx
@@ -1,7 +1,5 @@
+import { Button } from '@kosori/ui/button';
+
 export const ButtonDisabledDemo = () => {
-  return (
-    <div>
-      <p>button disabled demo</p>
-    </div>
-  );
+  return <Button disabled>Click me</Button>;
 };
