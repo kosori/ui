bf5a1343f3eb7e3be824e5b68b23fc6b80fb7e5e
Alexis Guzman <codingcodax@gmail.com>
2024-09-17 18:26:37 -0600

    feat(components): align items to the end

--- apps/www/src/components/demos/Badge/BadgeSizes.tsx
+++ apps/www/src/components/demos/Badge/BadgeSizes.tsx
@@ -2,36 +2,10 @@ import { Badge } from '@kosori/ui/badge';
 
 export const BadgeSizesDemo = () => {
   return (
-    <div className='flex gap-4'>
-      <div className='flex flex-col gap-4'>
-        <Badge size='small'>Small</Badge>
-        <Badge size='medium'>Medium</Badge>
-        <Badge size='large'>Large</Badge>
-      </div>
-
-      <div className='flex flex-col gap-4'>
-        <Badge size='small' variant='soft'>
-          Small
-        </Badge>
-        <Badge size='medium' variant='soft'>
-          Medium
-        </Badge>
-        <Badge size='large' variant='soft'>
-          Large
-        </Badge>
-      </div>
-
-      <div className='flex flex-col gap-4'>
-        <Badge size='small' variant='outline'>
-          Small
-        </Badge>
-        <Badge size='medium' variant='outline'>
-          Medium
-        </Badge>
-        <Badge size='large' variant='outline'>
-          Large
-        </Badge>
-      </div>
+    <div className='flex items-end gap-4'>
+      <Badge size='small'>Small</Badge>
+      <Badge size='medium'>Medium</Badge>
+      <Badge size='large'>Large</Badge>
     </div>
   );
 };
