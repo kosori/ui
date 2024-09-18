9c559599846c7c62bb4736d58874e61669b157ea
Alexis Guzman <codingcodax@gmail.com>
2024-09-18 10:43:49 -0600

    feat(components): add descriptive text to badge intents demo

--- apps/www/src/components/demos/Badge/BadgeIntents.tsx
+++ apps/www/src/components/demos/Badge/BadgeIntents.tsx
@@ -2,46 +2,52 @@ import { Badge } from '@kosori/ui/badge';
 
 export const BadgeIntentsDemo = () => {
   return (
-    <div className='flex gap-4'>
-      <div className='flex flex-col gap-4'>
-        <Badge>Default</Badge>
-        <Badge intent='info'>Info</Badge>
-        <Badge intent='success'>Success</Badge>
-        <Badge intent='warning'>Warning</Badge>
-        <Badge intent='error'>Error</Badge>
-      </div>
+    <div className='grid grid-cols-4 gap-4'>
+      <div />
+      <p className='text-sm text-grey-text'>Default</p>
+      <p className='text-sm text-grey-text'>Soft</p>
+      <p className='text-sm text-grey-text'>Outline</p>
 
-      <div className='flex flex-col gap-4'>
-        <Badge variant='soft'>Default</Badge>
-        <Badge intent='info' variant='soft'>
-          Info
-        </Badge>
-        <Badge intent='success' variant='soft'>
-          Success
-        </Badge>
-        <Badge intent='warning' variant='soft'>
-          Warning
-        </Badge>
-        <Badge intent='error' variant='soft'>
-          Error
-        </Badge>
-      </div>
+      <p className='text-sm text-grey-text'>Default</p>
+      <Badge>Badge</Badge>
+      <Badge variant='soft'>Badge</Badge>
+      <Badge variant='outline'>Badge</Badge>
 
-      <div className='flex flex-col gap-4'>
-        <Badge variant='outline'>Default</Badge>
-        <Badge intent='info' variant='outline'>
-          Info
-        </Badge>
-        <Badge intent='success' variant='outline'>
-          Success
-        </Badge>
-        <Badge intent='warning' variant='outline'>
-          Warning
-        </Badge>
-        <Badge intent='error' variant='outline'>
-          Error
-        </Badge>
-      </div>
+      <p className='text-sm text-grey-text'>Info</p>
+      <Badge intent='info'>Badge</Badge>
+      <Badge intent='info' variant='soft'>
+        Badge
+      </Badge>
+      <Badge intent='info' variant='outline'>
+        Badge
+      </Badge>
+
+      <p className='text-sm text-grey-text'>Success</p>
+      <Badge intent='success'>Badge</Badge>
+      <Badge intent='success' variant='soft'>
+        Badge
+      </Badge>
+      <Badge intent='success' variant='outline'>
+        Badge
+      </Badge>
+
+      <p className='text-sm text-grey-text'>Warning</p>
+      <Badge intent='warning'>Badge</Badge>
+      <Badge intent='warning' variant='soft'>
+        Badge
+      </Badge>
+      <Badge intent='warning' variant='outline'>
+        Badge
+      </Badge>
+
+      <p className='text-sm text-grey-text'>Error</p>
+      <Badge intent='error'>Badge</Badge>
+      <Badge intent='error' variant='soft'>
+        Badge
+      </Badge>
+      <Badge intent='error' variant='outline'>
+        Badge
+      </Badge>
     </div>
   );
 };
