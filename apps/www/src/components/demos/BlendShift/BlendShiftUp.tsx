a6e25accb5ec5ecd0b5827e0e3a4a41e3c2062f0
Alexis Guzman <codingcodax@gmail.com>
2024-09-18 18:23:04 -0600

    feat(components): setup blend shift up demo

--- apps/www/src/components/demos/BlendShift/BlendShiftUp.tsx
+++ apps/www/src/components/demos/BlendShift/BlendShiftUp.tsx
@@ -1,7 +1,16 @@
+import { clsx } from 'clsx/lite';
+
 export const BlendShiftUp = () => {
   return (
-    <div>
-      <p>blend shift up</p>
-    </div>
+    <a
+      className={clsx(
+        'relative text-grey-text-contrast',
+        'after:absolute after:-bottom-[4px] after:-left-[10%] after:h-0 after:w-[120%] after:bg-grey-base after:mix-blend-exclusion after:transition-all after:duration-300',
+        'hover:after:h-[120%]',
+      )}
+      href='#'
+    >
+      Blend Shift Up
+    </a>
   );
 };
