6314817d87f8a8ad97fe94358ce08effa2d895eb
Alexis Guzman <codingcodax@gmail.com>
2024-09-19 18:13:13 -0600

    feat(config): add mini mode

--- apps/www/src/config/modes.ts
+++ apps/www/src/config/modes.ts
@@ -1,5 +1,5 @@
 import type { IconProps } from '@radix-ui/react-icons/dist/types';
-import { ContainerIcon, CubeIcon } from '@radix-ui/react-icons';
+import { ContainerIcon, CubeIcon, MixIcon } from '@radix-ui/react-icons';
 
 import ui from '../../../../packages/ui/package.json';
 
@@ -19,7 +19,7 @@ export const modes: Mode[] = [
     param: 'ui',
     name: 'UI',
     package: '@kosori/ui',
-    description: 'UI components',
+    description: 'Core components',
     version: ui.version,
     icon: CubeIcon,
   },
@@ -27,8 +27,16 @@ export const modes: Mode[] = [
     param: 'hooks',
     name: 'Hooks',
     package: '@kosori/hooks',
-    description: 'Custom hooks',
+    description: 'React custom hooks',
     version: '0.1.0',
     icon: ContainerIcon,
+  },
+  {
+    param: 'mini',
+    name: 'Mini',
+    package: '@kosori/mini',
+    description: 'Micro components',
+    version: '0.1.0',
+    icon: MixIcon,
   },
 ];
