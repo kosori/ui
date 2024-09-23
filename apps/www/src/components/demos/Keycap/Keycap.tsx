1d5696120c7e5f929b540b18d2520ce52ad307b8
Alexis Guzman <codingcodax@gmail.com>
2024-09-23 17:55:07 -0600

    feat(components): setup keycap demo

--- apps/www/src/components/demos/Keycap/Keycap.tsx
+++ apps/www/src/components/demos/Keycap/Keycap.tsx
@@ -1,7 +1,59 @@
+import { clsx } from 'clsx/lite';
+
 export const KeycapDemo = () => {
   return (
-    <div>
-      <p>keycap</p>
+    <div className='flex gap-2'>
+      <button
+        className={clsx(
+          'flex h-9 w-9 items-center justify-center rounded-lg border border-b-4 border-grey-line border-b-grey-text-contrast bg-grey-bg text-sm font-semibold text-grey-text-contrast transition-[border] duration-100 ease-in-out',
+          'hover:border-b-[1px] hover:border-grey-line',
+        )}
+      >
+        ⌃
+      </button>
+
+      <button
+        className={clsx(
+          'flex h-9 w-9 items-center justify-center rounded-lg border border-b-4 border-grey-line border-b-grey-text-contrast bg-grey-bg text-sm font-semibold text-grey-text-contrast transition-[border] duration-100 ease-in-out',
+          'hover:border-b-[1px] hover:border-grey-line',
+        )}
+      >
+        ⌥
+      </button>
+
+      <button
+        className={clsx(
+          'flex h-9 w-9 items-center justify-center rounded-lg border border-b-4 border-grey-line border-b-grey-text-contrast bg-grey-bg text-sm font-semibold text-grey-text-contrast transition-[border] duration-100 ease-in-out',
+          'hover:border-b-[1px] hover:border-grey-line',
+        )}
+      >
+        ⌘
+      </button>
+
+      <button
+        className={clsx(
+          'flex h-9 w-24 items-center justify-center rounded-lg border border-b-4 border-grey-line border-b-grey-text-contrast bg-grey-bg text-sm font-semibold text-grey-text-contrast transition-[border] duration-100 ease-in-out',
+          'hover:border-b-[1px] hover:border-grey-line',
+        )}
+      />
+
+      <button
+        className={clsx(
+          'flex h-9 w-9 items-center justify-center rounded-lg border border-b-4 border-grey-line border-b-grey-text-contrast bg-grey-bg text-sm font-semibold text-grey-text-contrast transition-[border] duration-100 ease-in-out',
+          'hover:border-b-[1px] hover:border-grey-line',
+        )}
+      >
+        ⌘
+      </button>
+
+      <button
+        className={clsx(
+          'flex h-9 w-9 items-center justify-center rounded-lg border border-b-4 border-grey-line border-b-grey-text-contrast bg-grey-bg text-sm font-semibold text-grey-text-contrast transition-[border] duration-100 ease-in-out',
+          'hover:border-b-[1px] hover:border-grey-line',
+        )}
+      >
+        ⌥
+      </button>
     </div>
   );
 };
