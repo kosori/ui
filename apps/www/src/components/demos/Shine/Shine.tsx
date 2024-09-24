30ab34178c62be076c69e7c05676b0c96089e820
Alexis Guzman <codingcodax@gmail.com>
2024-09-24 17:36:18 -0600

    feat(components): setup shine demo

--- apps/www/src/components/demos/Shine/Shine.tsx
+++ apps/www/src/components/demos/Shine/Shine.tsx
@@ -1,7 +1,17 @@
+import { clsx } from 'clsx/lite';
+
 export const ShineDemo = () => {
   return (
-    <div>
-      <p>shine</p>
+    <div className='flex gap-2'>
+      <button
+        className={clsx(
+          'relative flex h-9 items-center justify-center overflow-clip rounded-lg bg-grey-text-contrast px-3 text-sm font-semibold text-grey-base',
+          'after:absolute after:-bottom-1/2 after:-top-1/2 after:w-3 after:bg-grey-base/20 after:transition-transform after:duration-700 after:ease-in-out after:[transform:translate3d(-600%,0,0)_rotate(35deg)]',
+          'hover:after:[transform:translate3d(600%,0,0)_rotate(35deg)]',
+        )}
+      >
+        Shine
+      </button>
     </div>
   );
 };
