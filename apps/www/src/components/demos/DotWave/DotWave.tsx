e02a425bc05cff6b4130bb28c2a7b0aa428fed2b
Alexis Guzman <codingcodax@gmail.com>
2024-09-26 11:50:53 -0600

    style(prettier): run `format:fix` script

--- apps/www/src/components/demos/DotWave/DotWave.tsx
+++ apps/www/src/components/demos/DotWave/DotWave.tsx
@@ -1,10 +1,10 @@
 export const DotWaveDemo = () => {
   return (
     <div className='flex gap-x-2'>
-      <div className='animate-jump size-2 rounded-full bg-grey-text-contrast will-change-transform [animation-delay:0.55s]' />
-      <div className='animate-jump size-2 rounded-full bg-grey-text-contrast will-change-transform [animation-delay:0.7s]' />
-      <div className='animate-jump size-2 rounded-full bg-grey-text-contrast will-change-transform [animation-delay:0.855s]' />
-      <div className='animate-jump size-2 rounded-full bg-grey-text-contrast will-change-transform' />
+      <div className='size-2 animate-jump rounded-full bg-grey-text-contrast will-change-transform [animation-delay:0.55s]' />
+      <div className='size-2 animate-jump rounded-full bg-grey-text-contrast will-change-transform [animation-delay:0.7s]' />
+      <div className='size-2 animate-jump rounded-full bg-grey-text-contrast will-change-transform [animation-delay:0.855s]' />
+      <div className='size-2 animate-jump rounded-full bg-grey-text-contrast will-change-transform' />
     </div>
   );
 };
--- apps/www/src/components/demos/Wobble/Wobble.tsx
+++ apps/www/src/components/demos/Wobble/Wobble.tsx
@@ -5,7 +5,7 @@ export const WobbleDemo = () => {
     <div
       className={clsx(
         'relative flex w-14 items-center justify-start',
-        'before:animate-wobble before:size-2 before:translate-x-full before:rounded-full before:bg-grey-text-contrast',
+        'before:size-2 before:translate-x-full before:animate-wobble before:rounded-full before:bg-grey-text-contrast',
       )}
     />
   );
