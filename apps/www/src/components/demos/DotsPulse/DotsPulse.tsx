44c7ec45e8474e01bfc8851ddade1d8bba02b719
Alexis Guzman <codingcodax@gmail.com>
2024-09-25 18:40:23 -0600

    style(prettier): run `format:fix` script

--- apps/www/src/components/demos/DotsPulse/DotsPulse.tsx
+++ apps/www/src/components/demos/DotsPulse/DotsPulse.tsx
@@ -1,9 +1,9 @@
 export const DotsPulseDemo = () => {
   return (
     <div className='space-x-1.5'>
-      <span className='animate-flash inline-block size-2 rounded-full bg-grey-text-contrast' />
-      <span className='animate-flash inline-block size-2 rounded-full bg-grey-text-contrast [animation-delay:0.2s]' />
-      <span className='animate-flash inline-block size-2 rounded-full bg-grey-text-contrast [animation-delay:0.4s]' />
+      <span className='inline-block size-2 animate-flash rounded-full bg-grey-text-contrast' />
+      <span className='inline-block size-2 animate-flash rounded-full bg-grey-text-contrast [animation-delay:0.2s]' />
+      <span className='inline-block size-2 animate-flash rounded-full bg-grey-text-contrast [animation-delay:0.4s]' />
     </div>
   );
 };
