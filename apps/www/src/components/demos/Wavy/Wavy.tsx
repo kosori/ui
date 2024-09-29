7643a2066575c68ae7ae85e3c9cad0971f8ea4ed
Alexis Guzman <codingcodax@gmail.com>
2024-09-28 20:45:27 -0600

    feat(components): create wavy demo

---
+++ apps/www/src/components/demos/Wavy/Wavy.tsx
@@ -0,0 +1,16 @@
+import { clsx } from 'clsx/lite';
+
+export const WavyDemo = () => {
+  return (
+    <a
+      className={clsx(
+        'relative scale-x-100 text-grey-text-contrast transition-colors duration-300',
+        "before:absolute before:-bottom-3 before:left-0 before:h-full before:w-full before:origin-right before:scale-x-0 before:animate-[waving_3s_ease-in-out_infinite] before:bg-[url('/wavy.svg')] before:bg-[length:80px_80px] before:bg-[left_center] before:bg-repeat-x before:transition-transform before:duration-300",
+        'hover:text-grey-text hover:before:origin-left hover:before:scale-x-100',
+      )}
+      href=''
+    >
+      Wavy
+    </a>
+  );
+};
