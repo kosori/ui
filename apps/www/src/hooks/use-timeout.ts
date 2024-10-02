d722bde674eeced07f91e09974162de9efd5b064
Alexis Guzman <codingcodax@gmail.com>
2024-10-02 17:13:40 -0600

    feat(hooks): create use timeout

---
+++ apps/www/src/hooks/use-timeout.ts
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react';
+
+import { useIsomorphicLayoutEffect } from '~/hooks/use-isomorphic-layout-effect';
+
+/**
+ * A custom hook that sets a timeout to execute a callback function after a specified delay.
+ *
+ * @param {() => void} [callback] - The function to be executed after the delay.
+ * @param {number | null} [delay] - The time in milliseconds to wait before executing the callback. If null, the timeout will not be set.
+ *
+ * @example
+ * const [count, setCount] = useState(0);
+ * useTimeout(() => {
+ *   setCount(count + 1);
+ * }, 1000); // Increments count every second
+ *
+ * @returns {void} This hook does not return any value.
+ */
+export const useTimeout = (callback: () => void, delay: number | null) => {
+  const savedCallback = useRef(callback);
+
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (!delay && delay !== 0) {
+      return;
+    }
+
+    const id = setTimeout(() => {
+      savedCallback.current();
+    }, delay);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [delay]);
+};
