0ff0ab20427263f8ea5940d8a2319929f788e593
Alexis Guzman <codingcodax@gmail.com>
2024-10-03 14:00:41 -0600

    feat(components): replace width and height with size

--- apps/www/src/components/demos/Skeleton/Skeleton.tsx
+++ apps/www/src/components/demos/Skeleton/Skeleton.tsx
@@ -2,11 +2,13 @@ import { Skeleton } from '@kosori/ui/skeleton';
 
 export const SkeletonDemo = () => {
   return (
-    <div className='flex items-center space-x-4'>
-      <Skeleton className='h-12 w-12 rounded-full' />
-      <div className='space-y-2'>
-        <Skeleton className='h-4 w-[250px]' />
-        <Skeleton className='h-4 w-[200px]' />
+    <div className='flex flex-col gap-4'>
+      <div className='flex items-center gap-4'>
+        <Skeleton className='size-12 rounded-full' />
+        <div className='space-y-2'>
+          <Skeleton className='h-4 w-[250px]' />
+          <Skeleton className='h-4 w-[200px]' />
+        </div>
       </div>
     </div>
   );
