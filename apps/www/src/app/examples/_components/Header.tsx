1fcc52e9ac9d776d98f952f947e0cef5abc11ab0
Alexis Guzman <codingcodax@gmail.com>
2024-10-06 15:41:03 -0600

    feat(components): setup header

--- apps/www/src/app/examples/_components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <div>
-      <p>header</p>
-    </div>
-  );
-};
---
+++ apps/www/src/app/examples/faqs/_components/Header.tsx
@@ -0,0 +1,96 @@
+import Link from 'next/link';
+import { clsx } from 'clsx/lite';
+
+import { Button } from '@kosori/ui/button';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '@kosori/ui/navigation-menu';
+
+import { Logo } from '~/components/Layout/Logo';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className='max-w-container mx-auto flex size-full flex-row items-center justify-between gap-6 px-6 pt-4'>
+        <div className={clsx('inline-flex gap-8')}>
+          <Link
+            className={clsx(
+              'inline-flex items-center gap-2 font-semibold text-grey-base',
+              '[&_img]:size-9',
+              '[&>span]:hidden [&>span]:text-grey-text-contrast',
+              '[&>span]:min-[700px]:inline-block',
+              '[&>span>span]:hidden',
+            )}
+            href='/examples/faqs'
+          >
+            <Logo />
+          </Link>
+
+          <NavigationMenu className='hidden sm:inline-flex'>
+            <NavigationMenuList className='gap-4'>
+              <NavigationMenuItem>
+                <Link legacyBehavior passHref href='#'>
+                  <NavigationMenuLink
+                    className={clsx(
+                      'cursor-pointer text-sm',
+                      'hover:text-grey-text-contrast hover:underline',
+                    )}
+                  >
+                    Home
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <Link legacyBehavior passHref href='#'>
+                  <NavigationMenuLink
+                    className={clsx(
+                      'cursor-pointer text-sm',
+                      'hover:text-grey-text-contrast hover:underline',
+                    )}
+                  >
+                    Products
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <Link legacyBehavior passHref href='#'>
+                  <NavigationMenuLink
+                    className={clsx(
+                      'cursor-pointer text-sm',
+                      'hover:text-grey-text-contrast hover:underline',
+                    )}
+                  >
+                    Resources
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <Link legacyBehavior passHref href='#'>
+                  <NavigationMenuLink
+                    className={clsx(
+                      'cursor-pointer text-sm',
+                      'hover:text-grey-text-contrast hover:underline',
+                    )}
+                  >
+                    Pricing
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
+        <div className='inline-flex gap-4'>
+          <Button variant='outline'>Log in</Button>
+          <Button>Sign up</Button>
+        </div>
+      </div>
+    </header>
+  );
+};
