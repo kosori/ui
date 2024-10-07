8c5be541c068f917c1c00598affd01bdb230f7e9
Alexis Guzman <codingcodax@gmail.com>
2024-10-07 14:10:30 -0600

    feat(pages): show faqs example

--- apps/www/src/app/(marketing)/examples/page.tsx
+++ apps/www/src/app/(marketing)/examples/page.tsx
@@ -1,37 +1,40 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
 import { clsx } from 'clsx/lite';
-
-import { Button } from '@kosori/ui/button';
 
 const Examples: NextPage = () => {
   return (
-    <main className='flex min-h-[calc(100vh-56px)] flex-col items-center justify-center gap-3 p-8'>
-      <h1
-        className={clsx(
-          'bg-gradient-to-b from-grey-text-contrast to-grey-text bg-clip-text text-center text-4xl font-medium text-transparent',
-          'md:text-5xl',
-          'lg:text-6xl',
-        )}
-      >
-        Coming Soon
-      </h1>
-      <h2
-        className={clsx(
-          'text-center text-lg text-grey-text',
-          'sm:text-xl',
-          'md:text-2xl',
-        )}
-      >
-        In the meantime, check out some of the components already available.
-      </h2>
+    <div className='container'>
+      <main className='flex max-w-screen-lg flex-col gap-3 py-12'>
+        <h1
+          className={clsx(
+            'bg-gradient-to-b from-grey-text-contrast to-grey-text bg-clip-text text-4xl font-medium text-transparent',
+            'md:text-5xl',
+            'lg:text-6xl',
+          )}
+        >
+          UI Examples
+        </h1>
+        <h2
+          className={clsx(
+            'text-lg text-grey-text',
+            'sm:text-xl',
+            'md:text-2xl',
+          )}
+        >
+          Examples using the components available in Kõsori. Use them as a
+          reference or as a starting point for your own project.
+        </h2>
+      </main>
 
-      <div className='mt-6 flex gap-4'>
-        <Button asChild>
-          <Link href='/docs/ui/components'>Browse components</Link>
-        </Button>
+      <div className='aspect-video overflow-hidden rounded-lg border'>
+        <iframe
+          className='size-full'
+          loading='lazy'
+          src='/examples/faqs'
+          title='FAQs'
+        />
       </div>
-    </main>
+    </div>
   );
 };
 
