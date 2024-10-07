62840b5b05c8471c608344021185fd4683f4700a
Alexis Guzman <codingcodax@gmail.com>
2024-10-07 16:45:32 -0600

    feat(pages): add faqs heading

--- apps/www/src/app/(marketing)/examples/page.tsx
+++ apps/www/src/app/(marketing)/examples/page.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { clsx } from 'clsx/lite';
+
+import { Badge } from '@kosori/ui/badge';
 
 const Examples: NextPage = () => {
   return (
-    <div className='container'>
+    <div className='container pb-6'>
       <main className='flex max-w-screen-lg flex-col gap-3 py-12'>
         <h1
           className={clsx(
@@ -26,13 +29,44 @@ const Examples: NextPage = () => {
         </h2>
       </main>
 
-      <div className='aspect-video overflow-hidden rounded-lg border'>
-        <iframe
-          className='size-full'
-          loading='lazy'
-          src='/examples/faqs'
-          title='FAQs'
-        />
+      <div>
+        <h3 className='text-xl font-semibold text-grey-text-contrast'>FAQs</h3>
+        <div className='mb-6 flex gap-2'>
+          <Link href='/docs/ui/accordion'>
+            <Badge size='small' variant='outline'>
+              Accordion
+            </Badge>
+          </Link>
+          <Link href='/docs/ui/button'>
+            <Badge size='small' variant='outline'>
+              Button
+            </Badge>
+          </Link>
+          <Link href='/docs/ui/card'>
+            <Badge size='small' variant='outline'>
+              Card
+            </Badge>
+          </Link>
+          <Link href='/docs/ui/input'>
+            <Badge size='small' variant='outline'>
+              Input
+            </Badge>
+          </Link>
+          <Link href='/docs/ui/navigation-menu'>
+            <Badge size='small' variant='outline'>
+              Navigation Menu
+            </Badge>
+          </Link>
+        </div>
+
+        <div className='aspect-video resize overflow-auto rounded-lg border'>
+          <iframe
+            className='size-full'
+            loading='lazy'
+            src='/examples/faqs'
+            title='FAQs'
+          />
+        </div>
       </div>
     </div>
   );
