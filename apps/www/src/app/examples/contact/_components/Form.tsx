9bc2c1c2526ad133ae842cefb091ec972d8c21a6
Alexis Guzman <codingcodax@gmail.com>
2024-10-09 13:39:32 -0600

    feat(components): setup form

--- apps/www/src/app/examples/contact/_components/Form.tsx
+++ apps/www/src/app/examples/contact/_components/Form.tsx
@@ -1,7 +1,132 @@
-export const Form = () => {
+import { zodResolver } from '@hookform/resolvers/zod';
+import { clsx } from 'clsx/lite';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@kosori/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@kosori/ui/form';
+import { Input } from '@kosori/ui/input';
+import { Textarea } from '@kosori/ui/textarea';
+
+const FormSchema = z.object({
+  firstName: z.string().min(1, { message: 'Please enter your first name' }),
+  lastName: z.string().min(1, { message: 'Please enter your last name' }),
+  email: z.string().email({ message: 'Please enter a valid email' }),
+  phone: z.string().min(1, { message: 'Please enter your phone number' }),
+  message: z
+    .string()
+    .min(1, { message: 'Please tell us what we can help you with' }),
+});
+
+export const ContactForm = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      message: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <p>form</p>
-    </div>
+    <section className={clsx('px-4 py-8', 'md:px-6', 'lg:px-8')}>
+      <Form {...form}>
+        <form
+          className='grid w-full gap-4'
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <div className='grid grid-cols-2 gap-4'>
+            <FormField
+              control={form.control}
+              name='firstName'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First name</FormLabel>
+                  <FormControl>
+                    <Input placeholder='First name' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='lastName'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last name</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Last name' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder='Email' type='email' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='phone'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone number</FormLabel>
+                <FormControl>
+                  <Input placeholder='Phone number' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='message'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className='max-h-[140px] min-h-[120px] resize-y'
+                    placeholder='Tell us what we can help you with'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className='!mt-8 w-full'>Send message</Button>
+        </form>
+      </Form>
+    </section>
   );
 };
