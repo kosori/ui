0730785ba7d713bd7bbbd97f86bc946e68eebcb4
Alexis Guzman <codingcodax@gmail.com>
2024-10-11 14:30:03 -0600

    feat(layout): add theme nav

--- apps/www/src/app/(marketing)/layout.tsx
+++ apps/www/src/app/(marketing)/layout.tsx
@@ -1,11 +1,17 @@
 import { HomeLayout } from 'fumadocs-ui/home-layout';
 
+import { ThemeNav } from '~/components/Layout/ThemeNav';
 import { baseOptions } from '~/config/layout';
 
 type Props = { children: React.ReactNode };
 
 const MarketingLayout = ({ children }: Props) => {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <HomeLayout {...baseOptions}>
+      {children}
+      <ThemeNav />
+    </HomeLayout>
+  );
 };
 
 export default MarketingLayout;
--- apps/www/src/app/docs/layout.tsx
+++ apps/www/src/app/docs/layout.tsx
@@ -1,8 +1,14 @@
 import type { ReactNode } from 'react';
 import { DocsLayout } from 'fumadocs-ui/layout';
 
+import { ThemeNav } from '~/components/Layout/ThemeNav';
 import { docsOptions } from '~/config/layout';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
+  return (
+    <DocsLayout {...docsOptions}>
+      {children}
+      <ThemeNav />
+    </DocsLayout>
+  );
 }
