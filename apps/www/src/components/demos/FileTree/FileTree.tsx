5419ceba9f91798105df9dcc723b7ce609d57a26
Alexis Guzman <codingcodax@gmail.com>
2024-10-11 12:38:28 -0600

    feat(components): create file tree demo

---
+++ apps/www/src/components/demos/FileTree/FileTree.tsx
@@ -0,0 +1,43 @@
+import {
+  File,
+  FileTree,
+  Folder,
+  FolderFiles,
+  FolderName,
+} from '@kosori/ui/file-tree';
+
+export const FileTreeDemo = () => {
+  return (
+    <FileTree className='w-full max-w-xs'>
+      <Folder>
+        <FolderName>app</FolderName>
+
+        <FolderFiles>
+          <Folder>
+            <FolderName>components</FolderName>
+
+            <FolderFiles>
+              <File>button.tsx</File>
+              <File>card.tsx</File>
+            </FolderFiles>
+          </Folder>
+
+          <File>layout.tsx</File>
+          <File>global.css</File>
+          <File>page.tsx</File>
+        </FolderFiles>
+      </Folder>
+
+      <Folder>
+        <FolderName>public</FolderName>
+
+        <FolderFiles>
+          <File>favicon.ico</File>
+          <File>index.html</File>
+        </FolderFiles>
+      </Folder>
+
+      <File>package.json</File>
+    </FileTree>
+  );
+};
