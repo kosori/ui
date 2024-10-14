87dd4a9c6a7c5344684947d7d995d46f27e37e62
Alexis Guzman <codingcodax@gmail.com>
2024-10-14 12:56:31 -0600

    feat(utils): add parser prop

--- packages/cli/src/utils/buildPrettifier.ts
+++ packages/cli/src/utils/buildPrettifier.ts
@@ -1,13 +1,16 @@
+import type { BuiltInParserName, LiteralUnion } from 'prettier';
 import prettier from '@prettier/sync';
 
-export const buildPrettifier = () => {
+type Props = { parser?: LiteralUnion<BuiltInParserName, string> };
+
+export const buildPrettifier = ({ parser }: Props) => {
   const config = prettier.resolveConfig(process.cwd()) ?? {
     singleQuote: true,
     tabWidth: 2,
     trailingComma: 'es5',
   };
 
-  config.parser = config.parser ?? 'babel';
+  config.parser = config.parser ?? parser ?? 'babel';
 
   return (text: string) => prettier.format(text, config);
 };
