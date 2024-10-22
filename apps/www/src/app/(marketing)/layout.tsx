209ca7ee30775c685ee3e8331bdccf8c9d91129c
Alexis Guzman <codingcodax@gmail.com>
2024-10-22 12:38:10 -0600

    fix: update `fumadocs-ui` layout imports

--- apps/www/src/app/(marketing)/layout.tsx
+++ apps/www/src/app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react';
-import { HomeLayout } from 'fumadocs-ui/home-layout';
+import { HomeLayout } from 'fumadocs-ui/layouts/home';
 
 import { ThemeNav } from '~/components/Layout/ThemeNav';
 import { baseOptions } from '~/config/layout';
