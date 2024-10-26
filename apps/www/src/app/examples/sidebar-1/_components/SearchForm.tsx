897594d7b797c79c513b707a3cb53cbdf2d78624
Alexis Guzman <codingcodax@gmail.com>
2024-10-26 11:06:15 -0600

    feat(components): create search form

--- apps/www/src/app/examples/sidebar-1/_components/SearchForm.tsx
+++ apps/www/src/app/examples/sidebar-1/_components/SearchForm.tsx
@@ -1,7 +1,30 @@
-export const SearchForm = () => {
+import { Search } from 'lucide-react';
+
+import { Label } from '@kosori/ui/label';
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarInput,
+} from '@kosori/ui/sidebar';
+
+type Props = React.ComponentProps<'form'>;
+
+export const SearchForm = ({ ...props }: Props) => {
   return (
-    <div>
-      <p>search form</p>
-    </div>
+    <form {...props}>
+      <SidebarGroup className='py-0'>
+        <SidebarGroupContent className='relative'>
+          <Label className='sr-only' htmlFor='search'>
+            Search
+          </Label>
+          <SidebarInput
+            className='pl-8'
+            id='search'
+            placeholder='Search the docs...'
+          />
+          <Search className='pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50' />
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </form>
   );
 };
