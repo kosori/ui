53901344a12ac756ab02fb3bbb657f10fc6c46e9
Alexis Guzman <codingcodax@gmail.com>
2024-10-28 17:44:34 -0600

    feat(components): setup date picker

--- apps/www/src/app/examples/sidebar-12/_components/DatePicker.tsx
+++ apps/www/src/app/examples/sidebar-12/_components/DatePicker.tsx
@@ -1,7 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Calendar } from '@kosori/ui/calendar';
+import { SidebarGroup, SidebarGroupContent } from '@kosori/ui/sidebar';
+
 export const DatePicker = () => {
+  const [selected, setSelected] = useState<Date | undefined>(new Date());
+
   return (
-    <div>
-      <p>date picker</p>
-    </div>
+    <SidebarGroup className='px-0'>
+      <SidebarGroupContent>
+        <Calendar
+          className='[&_table]:w-full [&_tr]:justify-between'
+          mode='single'
+          selected={selected}
+          onSelect={setSelected}
+        />
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 };
