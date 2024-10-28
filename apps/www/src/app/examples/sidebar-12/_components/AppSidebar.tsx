090c5e4ded66fc6d2364fa351cc920caf04e0ce5
Alexis Guzman <codingcodax@gmail.com>
2024-10-28 17:44:42 -0600

    feat(components): setup app sidebar

--- apps/www/src/app/examples/sidebar-12/_components/AppSidebar.tsx
+++ apps/www/src/app/examples/sidebar-12/_components/AppSidebar.tsx
@@ -1,7 +1,69 @@
-export const AppSidebar = () => {
+import * as React from 'react';
+import { Plus } from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarSeparator,
+} from '@kosori/ui/sidebar';
+
+import { Calendars } from './Calendars';
+import { DatePicker } from './DatePicker';
+import { NavUser } from './NavUser';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  calendars: [
+    {
+      name: 'My Calendars',
+      items: ['Personal', 'Work', 'Family'],
+    },
+    {
+      name: 'Favorites',
+      items: ['Holidays', 'Birthdays'],
+    },
+    {
+      name: 'Other',
+      items: ['Travel', 'Reminders', 'Deadlines'],
+    },
+  ],
+};
+
+type Props = React.ComponentProps<typeof Sidebar>;
+
+export const AppSidebar = ({ ...props }: Props) => {
   return (
-    <div>
-      <p>app sidebar</p>
-    </div>
+    <Sidebar {...props}>
+      <SidebarHeader className='h-16 border-b'>
+        <NavUser user={data.user} />
+      </SidebarHeader>
+      <SidebarContent>
+        <DatePicker />
+        <SidebarSeparator className='mx-0' />
+        <Calendars calendars={data.calendars} />
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <Plus />
+              <span>New Calendar</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
   );
 };
