8c310ef66c616868533fbb058e29a7d540a8039e
Alexis Guzman <codingcodax@gmail.com>
2024-10-28 10:50:12 -0600

    fix(pages): update background colors

--- apps/www/src/app/examples/sidebar-7/page.tsx
+++ apps/www/src/app/examples/sidebar-7/page.tsx
@@ -42,11 +42,11 @@ const Sidebar1 = () => {
           </header>
           <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
             <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-              <div className='bg-muted/50 aspect-video rounded-xl' />
-              <div className='bg-muted/50 aspect-video rounded-xl' />
-              <div className='bg-muted/50 aspect-video rounded-xl' />
+              <div className='aspect-video rounded-xl bg-grey-bg-subtle' />
+              <div className='aspect-video rounded-xl bg-grey-bg-subtle' />
+              <div className='aspect-video rounded-xl bg-grey-bg-subtle' />
             </div>
-            <div className='bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min' />
+            <div className='min-h-[100vh] flex-1 rounded-xl bg-grey-bg-subtle md:min-h-min' />
           </div>
         </SidebarInset>
       </SidebarProvider>
