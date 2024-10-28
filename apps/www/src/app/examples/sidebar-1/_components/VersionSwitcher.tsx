edf46cbc35ffa81075f7d4beb20e612932123fb0
Alexis Guzman <codingcodax@gmail.com>
2024-10-28 10:47:31 -0600

    feat(components): setup version switcher

--- apps/www/src/app/examples/sidebar-1/_components/VersionSwitcher.tsx
+++ apps/www/src/app/examples/sidebar-1/_components/VersionSwitcher.tsx
@@ -1,7 +1,63 @@
-export const VersionSwitcher = () => {
+'use client';
+
+import { useState } from 'react';
+import { Check, ChevronsUpDown, GalleryVerticalEnd } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@kosori/ui/dropdown-menu';
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@kosori/ui/sidebar';
+
+type Props = {
+  versions: string[];
+  defaultVersion: string;
+};
+
+export const VersionSwitcher = ({ versions, defaultVersion }: Props) => {
+  const [selectedVersion, setSelectedVersion] = useState(defaultVersion);
+
   return (
-    <div>
-      <p>version switcher</p>
-    </div>
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              className='data-[state=open]:text-grey-text-active data-[state=open]:bg-grey-bg-active'
+              size='large'
+            >
+              <div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-grey-text-contrast text-grey-base'>
+                <GalleryVerticalEnd className='size-4' />
+              </div>
+              <div className='flex flex-col gap-0.5 leading-none'>
+                <span className='font-semibold'>Documentation</span>
+                <span className=''>v{selectedVersion}</span>
+              </div>
+              <ChevronsUpDown className='ml-auto' />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align='start'
+            className='w-[--radix-dropdown-menu-trigger-width]'
+          >
+            {versions.map((version) => (
+              <DropdownMenuItem
+                key={version}
+                onSelect={() => setSelectedVersion(version)}
+              >
+                v{version}{' '}
+                {version === selectedVersion && <Check className='ml-auto' />}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
   );
 };
