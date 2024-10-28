4583ce6f476a6450f07acba4bcf1614f84ef5178
Alexis Guzman <codingcodax@gmail.com>
2024-10-28 17:58:46 -0600

    feat(pages): create sidebar 13

---
+++ apps/www/src/app/examples/sidebar-13/page.tsx
@@ -0,0 +1,15 @@
+import { SettingsDialog } from './_components/SettingsDialog';
+
+export const iframeHeight = '800px';
+
+export const description = 'A sidebar in a dialog.';
+
+const Sidebar13 = () => {
+  return (
+    <div className='flex h-svh items-center justify-center'>
+      <SettingsDialog />
+    </div>
+  );
+};
+
+export default Sidebar13;
