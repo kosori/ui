5a92ae511f1614d4328fd15a4fd1ac6bde05b68d
Alexis Guzman <codingcodax@gmail.com>
2024-10-28 13:26:32 -0600

    feat(components): setup nav workspaces

--- apps/www/src/app/examples/sidebar-10/_components/NavWorkspaces.tsx
+++ apps/www/src/app/examples/sidebar-10/_components/NavWorkspaces.tsx
@@ -1,7 +1,85 @@
-export const NavWorkspaces = () => {
+import { ChevronRight, MoreHorizontal, Plus } from 'lucide-react';
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@kosori/ui/collapsible';
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from '@kosori/ui/sidebar';
+
+type Props = {
+  workspaces: {
+    name: string;
+    emoji: React.ReactNode;
+    pages: {
+      name: string;
+      emoji: React.ReactNode;
+    }[];
+  }[];
+};
+
+export const NavWorkspaces = ({ workspaces }: Props) => {
   return (
-    <div>
-      <p>nav workspaces</p>
-    </div>
+    <SidebarGroup>
+      <SidebarGroupLabel>Workspaces</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {workspaces.map((workspace) => (
+            <Collapsible key={workspace.name}>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href='#'>
+                    <span>{workspace.emoji}</span>
+                    <span>{workspace.name}</span>
+                  </a>
+                </SidebarMenuButton>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuAction
+                    showOnHover
+                    className='left-2 bg-grey-bg text-grey-text-contrast data-[state=open]:rotate-90'
+                  >
+                    <ChevronRight />
+                  </SidebarMenuAction>
+                </CollapsibleTrigger>
+                <SidebarMenuAction showOnHover>
+                  <Plus />
+                </SidebarMenuAction>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {workspace.pages.map((page) => (
+                      <SidebarMenuSubItem key={page.name}>
+                        <SidebarMenuSubButton asChild>
+                          <a href='#'>
+                            <span>{page.emoji}</span>
+                            <span>{page.name}</span>
+                          </a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+          ))}
+          <SidebarMenuItem>
+            <SidebarMenuButton className='text-grey-text'>
+              <MoreHorizontal />
+              <span>More</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 };
