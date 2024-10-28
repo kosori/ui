dfcfe953cca24391c9118c3fcfe3fec0a7d2aca9
Alexis Guzman <codingcodax@gmail.com>
2024-10-28 13:26:46 -0600

    feat(components): setup nav favorites

--- apps/www/src/app/examples/sidebar-10/_components/NavFavorites.tsx
+++ apps/www/src/app/examples/sidebar-10/_components/NavFavorites.tsx
@@ -1,7 +1,94 @@
-export const NavFavorites = () => {
+'use client';
+
+import {
+  ArrowUpRight,
+  Link,
+  MoreHorizontal,
+  StarOff,
+  Trash2,
+} from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@kosori/ui/dropdown-menu';
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from '@kosori/ui/sidebar';
+
+type Props = {
+  favorites: {
+    name: string;
+    url: string;
+    emoji: string;
+  }[];
+};
+
+export const NavFavorites = ({ favorites }: Props) => {
+  const { isMobile } = useSidebar();
+
   return (
-    <div>
-      <p>nav favorites</p>
-    </div>
+    <SidebarGroup className='group-data-[collapsible=icon]:hidden'>
+      <SidebarGroupLabel>Favorites</SidebarGroupLabel>
+      <SidebarMenu>
+        {favorites.map((item) => (
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton asChild>
+              <a href={item.url} title={item.name}>
+                <span>{item.emoji}</span>
+                <span>{item.name}</span>
+              </a>
+            </SidebarMenuButton>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuAction showOnHover>
+                  <MoreHorizontal />
+                  <span className='sr-only'>More</span>
+                </SidebarMenuAction>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                align={isMobile ? 'end' : 'start'}
+                className='w-56 rounded-lg'
+                side={isMobile ? 'bottom' : 'right'}
+              >
+                <DropdownMenuItem>
+                  <StarOff className='text-grey-text' />
+                  <span>Remove from Favorites</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link className='text-grey-text' />
+                  <span>Copy Link</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <ArrowUpRight className='text-grey-text' />
+                  <span>Open in New Tab</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Trash2 className='text-grey-text' />
+                  <span>Delete</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        ))}
+        <SidebarMenuItem>
+          <SidebarMenuButton className='text-grey-text'>
+            <MoreHorizontal />
+            <span>More</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarGroup>
   );
 };
