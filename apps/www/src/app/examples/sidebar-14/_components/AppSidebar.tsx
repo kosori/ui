f25afccfe7b5e2ff44c3630b756dfa598e36132e
Alexis Guzman <codingcodax@gmail.com>
2024-10-29 09:04:58 -0600

    feat(components): setup app sidebar

--- apps/www/src/app/examples/sidebar-14/_components/AppSidebar.tsx
+++ apps/www/src/app/examples/sidebar-14/_components/AppSidebar.tsx
@@ -1,7 +1,197 @@
-export const AppSidebar = () => {
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from '@kosori/ui/sidebar';
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: 'Getting Started',
+      url: '#',
+      items: [
+        {
+          title: 'Installation',
+          url: '#',
+        },
+        {
+          title: 'Project Structure',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Building Your Application',
+      url: '#',
+      items: [
+        {
+          title: 'Routing',
+          url: '#',
+        },
+        {
+          title: 'Data Fetching',
+          url: '#',
+          isActive: true,
+        },
+        {
+          title: 'Rendering',
+          url: '#',
+        },
+        {
+          title: 'Caching',
+          url: '#',
+        },
+        {
+          title: 'Styling',
+          url: '#',
+        },
+        {
+          title: 'Optimizing',
+          url: '#',
+        },
+        {
+          title: 'Configuring',
+          url: '#',
+        },
+        {
+          title: 'Testing',
+          url: '#',
+        },
+        {
+          title: 'Authentication',
+          url: '#',
+        },
+        {
+          title: 'Deploying',
+          url: '#',
+        },
+        {
+          title: 'Upgrading',
+          url: '#',
+        },
+        {
+          title: 'Examples',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'API Reference',
+      url: '#',
+      items: [
+        {
+          title: 'Components',
+          url: '#',
+        },
+        {
+          title: 'File Conventions',
+          url: '#',
+        },
+        {
+          title: 'Functions',
+          url: '#',
+        },
+        {
+          title: 'next.config.js Options',
+          url: '#',
+        },
+        {
+          title: 'CLI',
+          url: '#',
+        },
+        {
+          title: 'Edge Runtime',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Architecture',
+      url: '#',
+      items: [
+        {
+          title: 'Accessibility',
+          url: '#',
+        },
+        {
+          title: 'Fast Refresh',
+          url: '#',
+        },
+        {
+          title: 'Next.js Compiler',
+          url: '#',
+        },
+        {
+          title: 'Supported Browsers',
+          url: '#',
+        },
+        {
+          title: 'Turbopack',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Community',
+      url: '#',
+      items: [
+        {
+          title: 'Contribution Guide',
+          url: '#',
+        },
+      ],
+    },
+  ],
+};
+
+type Props = React.ComponentProps<typeof Sidebar>;
+
+export const AppSidebar = ({ ...props }: Props) => {
   return (
-    <div>
-      <p>app sidebar</p>
-    </div>
+    <Sidebar {...props}>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Table of Contents</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {data.navMain.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a className='font-medium' href={item.url}>
+                      {item.title}
+                    </a>
+                  </SidebarMenuButton>
+                  {item.items.length ? (
+                    <SidebarMenuSub>
+                      {item.items.map((item) => (
+                        <SidebarMenuSubItem key={item.title}>
+                          <SidebarMenuSubButton
+                            asChild
+                            isActive={item.isActive}
+                          >
+                            <a href={item.url}>{item.title}</a>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  ) : null}
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
   );
 };
