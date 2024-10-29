c12efe83681341a736dbb81c89d9c36877918801
Alexis Guzman <codingcodax@gmail.com>
2024-10-29 09:33:57 -0600

    feat(components): setup sidebar right

--- apps/www/src/app/examples/sidebar-15/_components/SidebarRight.tsx
+++ apps/www/src/app/examples/sidebar-15/_components/SidebarRight.tsx
@@ -1,7 +1,72 @@
-export const SidebarRight = () => {
+import { Plus } from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarSeparator,
+} from '@kosori/ui/sidebar';
+
+import { Calendars } from './Calendars';
+import { DatePicker } from './DatePicker';
+import { NavUser } from './NavUser';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  calendars: [
+    {
+      name: 'My Calendars',
+      items: ['Personal', 'Work', 'Family'],
+    },
+    {
+      name: 'Favorites',
+      items: ['Holidays', 'Birthdays'],
+    },
+    {
+      name: 'Other',
+      items: ['Travel', 'Reminders', 'Deadlines'],
+    },
+  ],
+};
+
+type Props = React.ComponentProps<typeof Sidebar>;
+
+export const SidebarRight = ({ ...props }: Props) => {
   return (
-    <div>
-      <p>sidebar right</p>
-    </div>
+    <Sidebar
+      className='sticky top-0 hidden h-svh border-l lg:flex'
+      collapsible='none'
+      {...props}
+    >
+      <SidebarHeader className='h-16 border-b'>
+        <NavUser user={data.user} />
+      </SidebarHeader>
+      <SidebarContent>
+        <DatePicker />
+        <SidebarSeparator className='mx-0' />
+        <Calendars calendars={data.calendars} />
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <Plus />
+              <span>New Calendar</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
   );
 };
