bfea69f5c7b60026fa317635430c2d0718241251
Alexis Guzman <codingcodax@gmail.com>
2024-10-29 09:05:28 -0600

    feat(components): setup nav secondary

--- apps/www/src/app/examples/sidebar-15/_components/NavSecondary.tsx
+++ apps/www/src/app/examples/sidebar-15/_components/NavSecondary.tsx
@@ -1,7 +1,41 @@
-export const NavSecondary = () => {
+import type { LucideIcon } from 'lucide-react';
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@kosori/ui/sidebar';
+
+type Props = {
+  items: {
+    title: string;
+    url: string;
+    icon: LucideIcon;
+    badge?: React.ReactNode;
+  }[];
+} & React.ComponentPropsWithoutRef<typeof SidebarGroup>;
+
+export const NavSecondary = ({ items, ...props }: Props) => {
   return (
-    <div>
-      <p>nav secondary</p>
-    </div>
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+              {item.badge && <SidebarMenuBadge>{item.badge}</SidebarMenuBadge>}
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 };
