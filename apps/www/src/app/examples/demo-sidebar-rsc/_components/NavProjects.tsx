dbf19792f3a9de2555c26a7d9bc3d98d7ab364c2
Alexis Guzman <codingcodax@gmail.com>
2024-11-04 15:47:39 -0600

    feat(components): setup nav projects

--- apps/www/src/app/examples/demo-sidebar-rsc/_components/NavProjects.tsx
+++ apps/www/src/app/examples/demo-sidebar-rsc/_components/NavProjects.tsx
@@ -1,7 +1,32 @@
-export const NavProjects = () => {
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@kosori/ui/sidebar';
+
+import { projects } from '../_config/projects';
+
+// Dummy fetch function
+const fetchProjects = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return projects;
+};
+
+export const NavProjects = async () => {
+  const projects = await fetchProjects();
+
   return (
-    <div>
-      <p>nav projects</p>
-    </div>
+    <SidebarMenu>
+      {projects.map((project) => (
+        <SidebarMenuItem key={project.name}>
+          <SidebarMenuButton asChild>
+            <a href={project.url}>
+              <project.icon />
+              <span>{project.name}</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </SidebarMenu>
   );
 };
