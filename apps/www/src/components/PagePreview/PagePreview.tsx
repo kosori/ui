0a597b5752cef3d8e3fdb46466e0a2cf4a6b84b1
Alexis Guzman <codingcodax@gmail.com>
2024-11-05 13:23:26 -0600

    feat(components): setup page preview

--- apps/www/src/components/PagePreview/PagePreview.tsx
+++ apps/www/src/components/PagePreview/PagePreview.tsx
@@ -1,7 +1,19 @@
-export const PagePreview = () => {
+type Props = {
+  type: 'example' | 'template';
+  name: string;
+  description: string;
+};
+
+export const PagePreview = ({ type, name, description }: Props) => {
   return (
-    <div>
-      <p>page preview</p>
-    </div>
+    <figure>
+      <div className='relative aspect-[4/2.5] w-full overflow-hidden rounded-xl border'>
+        <div className='absolute inset-0 w-[1600px] bg-grey-base'>
+          <iframe className='size-full' src={`/${type}s/${name}`} />
+        </div>
+      </div>
+
+      <figcaption>{description}</figcaption>
+    </figure>
   );
 };
