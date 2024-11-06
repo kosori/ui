c9db1fab323f3e074d2be22ea16917a9505717e5
Alexis Guzman <codingcodax@gmail.com>
2024-11-06 13:09:38 -0600

    feat(config): remove banner

--- apps/www/src/config/layout.tsx
+++ apps/www/src/config/layout.tsx
@@ -1,13 +1,11 @@
 import type { DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
 import type { HomeLayoutProps } from 'fumadocs-ui/layouts/home';
 import { CodeSandboxLogoIcon } from '@radix-ui/react-icons';
-import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle';
 import { AppWindowMacIcon, LayoutTemplateIcon } from 'lucide-react';
 
 import { source } from '~/app/source';
 import { Logo } from '~/components/Layout/Logo';
 import { NavModes } from '~/components/Layout/NavModes';
-import { modes } from '~/config/modes';
 
 // shared configuration
 export const baseOptions: HomeLayoutProps = {
@@ -43,24 +41,6 @@ export const docsOptions: DocsLayoutProps = {
   tree: source.pageTree,
   nav: {
     ...baseOptions.nav,
-    enableSearch: true,
     children: undefined,
-  },
-  sidebar: {
-    defaultOpenLevel: 0,
-    banner: (
-      <RootToggle
-        options={modes.map((mode) => ({
-          url: `/docs/${mode.param}`,
-          icon: (
-            <div className='flex items-center justify-center rounded-md bg-gradient-to-t from-grey-line to-grey-bg-subtle'>
-              <mode.icon className='size-8 shrink-0 rounded-md p-1.5' />
-            </div>
-          ),
-          title: mode.name,
-          description: mode.description,
-        }))}
-      />
-    ),
   },
 };
