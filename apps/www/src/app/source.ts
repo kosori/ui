4130be84be1d57f192ee297780855067e445146d
Alexis Guzman <codingcodax@gmail.com>
2024-11-06 12:59:30 -0600

    feat(fumadocs): add icons

--- apps/www/src/app/source.ts
+++ apps/www/src/app/source.ts
@@ -1,9 +1,18 @@
+import { createElement } from 'react';
 import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from 'fumadocs-mdx';
+import { icons } from 'lucide-react';
 
+import { IconContainer } from '~/components/IconContainer';
 import { docs, meta } from '../../.source';
 
 export const source = loader({
   baseUrl: '/docs',
+  icon(icon) {
+    if (icon && icon in icons)
+      return createElement(IconContainer, {
+        icon: icons[icon as keyof typeof icons],
+      });
+  },
   source: createMDXSource(docs, meta),
 });
