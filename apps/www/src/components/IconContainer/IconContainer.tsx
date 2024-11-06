c331dda15e9025e177e16ac4266187e26f6b523f
Alexis Guzman <codingcodax@gmail.com>
2024-11-06 12:59:20 -0600

    feat(components): setup icon container

--- apps/www/src/components/IconContainer/IconContainer.tsx
+++ apps/www/src/components/IconContainer/IconContainer.tsx
@@ -1,7 +1,25 @@
-export const IconContainer = () => {
+import type { LucideIcon } from 'lucide-react';
+import type { HTMLAttributes } from 'react';
+import { clsx } from 'clsx/lite';
+import { TerminalIcon } from 'lucide-react';
+
+type Props = HTMLAttributes<HTMLDivElement> & {
+  icon?: LucideIcon;
+};
+
+export const IconContainer = ({
+  icon: Icon,
+  ...props
+}: Props): React.ReactElement => {
   return (
-    <div>
-      <p>icon container</p>
+    <div
+      {...props}
+      className={clsx(
+        'rounded-md border bg-gradient-to-t from-grey-line to-grey-bg-subtle p-0.5 shadow-md',
+        props.className,
+      )}
+    >
+      {Icon ? <Icon /> : <TerminalIcon />}
     </div>
   );
 };
