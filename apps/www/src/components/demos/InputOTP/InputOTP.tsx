f1f08703dc843a4b235a1cacb52feb97b0d63be9
Alexis Guzman <codingcodax@gmail.com>
2024-12-18 20:53:17 -0600

    fix(demos): update input otp

--- apps/www/src/components/demos/InputOTP/InputOTP.tsx
+++ apps/www/src/components/demos/InputOTP/InputOTP.tsx
@@ -9,23 +9,18 @@ import {
 
 export const InputOTPDemo = () => {
   return (
-    <InputOTP
-      maxLength={6}
-      render={({ slots }) => (
-        <>
-          <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-          </InputOTPGroup>
-          <InputOTPSeparator />
-          <InputOTPGroup>
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-          </InputOTPGroup>
-        </>
-      )}
-    />
+    <InputOTP maxLength={6}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+        <InputOTPSlot index={2} />
+      </InputOTPGroup>
+      <InputOTPSeparator />
+      <InputOTPGroup>
+        <InputOTPSlot index={3} />
+        <InputOTPSlot index={4} />
+        <InputOTPSlot index={5} />
+      </InputOTPGroup>
+    </InputOTP>
   );
 };
