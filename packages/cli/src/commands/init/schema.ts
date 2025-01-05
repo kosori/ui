27d59d60f175cfb8a7b8fc8424d68ac4ed03487a
Alexis Guzman <codingcodax@gmail.com>
2025-01-04 21:52:29 -0600

    feat(schemas): add `force` boolean

--- packages/cli/src/commands/init/schema.ts
+++ packages/cli/src/commands/init/schema.ts
@@ -2,8 +2,10 @@ import { z } from 'zod';
 
 export const typeSchema = z.literal('ui').or(z.literal('template'));
 
-export const initOptionsSchema = z.object({
+export type InitOptions = z.infer<typeof InitOptions>;
+export const InitOptions = z.object({
   yes: z.boolean(),
+  force: z.boolean(),
   cwd: z.string(),
 });
 
