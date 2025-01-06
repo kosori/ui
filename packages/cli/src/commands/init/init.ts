2f748a3d2f93693f7372997db9be6479e6df6216
Alexis Guzman <codingcodax@gmail.com>
2025-01-05 21:21:21 -0600

    feat(commands): add create or load project config function

--- packages/cli/src/commands/init/init.ts
+++ packages/cli/src/commands/init/init.ts
@@ -1,8 +1,11 @@
+import { promises as fs } from 'fs';
+import path from 'path';
 import * as p from '@clack/prompts';
 import { Command } from 'commander';
 
 import { handleError } from '~/utils/handle-error';
 import { highlighter } from '~/utils/highlighter';
+import { createOrLoadProjectConfig, loadProjectConfig } from './helpers/config';
 import { validateProject } from './helpers/pre-check';
 import { InitOptions } from './schema';
 
@@ -34,5 +37,30 @@ export const init = new Command()
   });
 
 const runInit = async (options: InitOptions) => {
-  await validateProject(options);
+  const { projectInfo } = await validateProject(options);
+
+  const { resolvedPaths: _, ...projectConfig } =
+    await createOrLoadProjectConfig(options.cwd);
+
+  if (!options.yes) {
+    const shouldContinue = await p.confirm({
+      message: `Write configuration to ${highlighter.info('"kosori.config.json"')}. Proceed?`,
+    });
+
+    if (p.isCancel(shouldContinue) || !shouldContinue) {
+      p.cancel('The configuration was not written.');
+      process.exit(0);
+    }
+  }
+
+  await p.tasks([
+    {
+      title: 'Writing kosori.config.json',
+      task: async () => {
+        const file = path.resolve(options.cwd, 'kosori.config.json');
+        const data = JSON.stringify(projectConfig, null, 2);
+        await fs.writeFile(file, data, 'utf8');
+      },
+    },
+  ]);
 };
