1980a1d46f72c0fe2683e1abb017de61c541af90
Alexis Guzman <codingcodax@gmail.com>
2025-01-19 13:06:01 -0600

    chore(components): simplified

--- apps/www/src/components/icon-container.tsx
+++ apps/www/src/components/icon-container.tsx
@@ -1,6 +1,5 @@
 import type { LucideIcon } from 'lucide-react';
 import type { HTMLAttributes } from 'react';
-import { clsx } from 'clsx/lite';
 import { TerminalIcon } from 'lucide-react';
 
 type Props = HTMLAttributes<HTMLDivElement> & {
@@ -11,15 +10,5 @@ export const IconContainer = ({
   icon: Icon,
   ...props
 }: Props): React.ReactElement => {
-  return (
-    <div
-      {...props}
-      className={clsx(
-        'rounded-md border bg-gradient-to-t from-grey-line to-grey-bg-subtle p-0.5 shadow-md',
-        props.className,
-      )}
-    >
-      {Icon ? <Icon /> : <TerminalIcon />}
-    </div>
-  );
+  return <div {...props}>{Icon ? <Icon /> : <TerminalIcon />}</div>;
 };
