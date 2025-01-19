7f2c119825dc43262cc1cd163a98fb520a228504
Alexis Guzman <codingcodax@gmail.com>
2025-01-19 13:06:57 -0600

    refactor(layout): replace with custom body

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata, Viewport } from 'next';
 import { clsx } from 'clsx/lite';
-import { GeistMono } from 'geist/font/mono';
-import { GeistSans } from 'geist/font/sans';
-
-import { defaultConfig } from '~/config/theme';
 
 import '~/app/globals.css';
 
 import type { PropsWithChildren } from 'react';
 
 import { ThemeLoader } from '~/components/Layout/ThemeLoader';
+import { Body } from './_components/body';
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
@@ -66,32 +63,15 @@ export const viewport: Viewport = {
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html suppressHydrationWarning lang='en'>
-      <body
-        className={clsx(
-          'min-h-screen font-sans antialiased',
-          GeistSans.variable,
-          GeistMono.variable,
-        )}
-        data-error-color={defaultConfig['error-color']}
-        data-grey-color={defaultConfig['grey-color']}
-        data-info-color={defaultConfig['info-color']}
-        data-primary-color={defaultConfig['primary-color']}
-        data-radius={defaultConfig.radius}
-        data-success-color={defaultConfig['success-color']}
-        data-warning-color={defaultConfig['warning-color']}
-        kosori-theme-wrapper=''
-      >
+      <Body>
         <Providers>
-          <div
-            className='min-h-screen bg-grey-base text-grey-text'
-            vaul-drawer-wrapper=''
-          >
+          <div className='' vaul-drawer-wrapper=''>
             {children}
           </div>
 
           <ThemeLoader />
         </Providers>
-      </body>
+      </Body>
     </html>
   );
 };
