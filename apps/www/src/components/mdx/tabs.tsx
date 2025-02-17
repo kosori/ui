2d0f6418a93a7ed6787db51139191918272ae98f
Alexis Guzman <codingcodax@gmail.com>
2025-02-16 21:00:18 -0600

    chore(components): remove kosori-cli-tabs

--- apps/www/src/components/mdx/tabs.tsx
+++ apps/www/src/components/mdx/tabs.tsx
@@ -6,6 +6,7 @@ type Props = React.ComponentPropsWithoutRef<typeof Tabs>;
 export const InstallTabs = (props: Props) => {
   return (
     <Tabs
+      persist
       className={clsx(
         'border-none bg-grey-base',
         '[&>div:first-child]:rounded-xl [&>div:first-child]:border',
@@ -13,17 +14,7 @@ export const InstallTabs = (props: Props) => {
         '[&>*:not(:first-child):not(:last-child)]:mt-8',
         '[&>*:not(:first-child)]:p-0',
       )}
-      {...props}
-    />
-  );
-};
-
-export const KosoriCLITabs = (props: Props) => {
-  return (
-    <Tabs
-      persist
-      groupId='kosori-cli'
-      items={['npm', 'pnpm', 'yarn', 'bun']}
+      groupId='kosori-install'
       {...props}
     />
   );
